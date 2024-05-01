67755011283a56d4879c1bf9937ce86f3723436c
ChaeAg <dydkglel123@gmail.com>
2024-05-01 14:18:36 +0900

    [INIT] 프로젝트 기초 세팅

---
+++ src/main/java/org/websoso/WSSServer/WssServerApplication.java
@@ -0,0 +1,12 @@
+package org.websoso.WSSServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WssServerApplication.class, args);
+	}
+}
---
+++ src/test/java/org/websoso/WSSServer/WssServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.websoso.WSSServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WssServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
