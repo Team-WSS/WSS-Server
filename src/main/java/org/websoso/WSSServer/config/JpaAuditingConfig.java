f8958b75234f329a75dfcc2fee0318777141a887
Kim-TaeUk <xodnr0130@naver.com>
2024-05-04 00:53:12 +0900

    [CHORE] JPA Auditing을 위한 configuration 추가

---
+++ src/main/java/org/websoso/WSSServer/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package org.websoso.WSSServer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}
