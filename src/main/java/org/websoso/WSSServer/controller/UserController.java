bbbaf02b69a13b4acb620985ff106721c4d2c3fd
Kim-TaeUk <xodnr0130@naver.com>
2024-05-08 02:43:54 +0900

    [FEAT] 닉네임 중복 확인 기능 구현
    중복인 경우 409 Conflict, 중복이 아닌 경우 200 OK

--- src/main/java/org/websoso/WSSServer/controller/UserController.java
+++ src/main/java/org/websoso/WSSServer/controller/UserController.java
@@ -1,11 +1,35 @@
 package org.websoso.WSSServer.controller;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.OK;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.websoso.WSSServer.dto.User.NicknameValidation;
+import org.websoso.WSSServer.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/nickname/check")
+    public ResponseEntity<NicknameValidation> checkNicknameAvailability(
+            @RequestParam("nickname") String nickname) {
+        NicknameValidation nicknameValidation = userService.isNicknameAvailable(nickname);
+        if (nicknameValidation.isDuplicated()) {
+            return ResponseEntity
+                    .status(CONFLICT)
+                    .body(nicknameValidation);
+        }
+        return ResponseEntity
+                .status(OK)
+                .body(nicknameValidation);
+    }
 }
--- src/main/java/org/websoso/WSSServer/service/UserService.java
+++ src/main/java/org/websoso/WSSServer/service/UserService.java
@@ -2,9 +2,17 @@ package org.websoso.WSSServer.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.websoso.WSSServer.dto.User.NicknameValidation;
+import org.websoso.WSSServer.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public NicknameValidation isNicknameAvailable(String nickname) {
+        boolean isNicknameTaken = userRepository.existsByNickname(nickname);
+        return NicknameValidation.of(isNicknameTaken);
+    }
 }
