9b8ffd8d74e6147c5230f8cf0e14273830eaf7af
Kim-TaeUk <xodnr0130@naver.com>
2024-05-09 03:17:40 +0900

    [FIX] nickname 중복 체크 메서드 읽기 전용으로 변경

--- src/main/java/org/websoso/WSSServer/service/UserService.java
+++ src/main/java/org/websoso/WSSServer/service/UserService.java
@@ -2,15 +2,18 @@ package org.websoso.WSSServer.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.websoso.WSSServer.dto.User.NicknameValidation;
 import org.websoso.WSSServer.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public NicknameValidation isNicknameAvailable(String nickname) {
         boolean isNicknameTaken = userRepository.existsByNickname(nickname);
         return NicknameValidation.of(isNicknameTaken);
