68cfff42bea6114431cc3863a83d553ac67f5b83
ChaeAg <dydkglel123@gmail.com>
2024-05-09 17:59:20 +0900

    [FEAT] 유저 인증/인가를 위한 Spring Security 설정 및 API 구현

--- src/main/java/org/websoso/WSSServer/controller/UserController.java
+++ src/main/java/org/websoso/WSSServer/controller/UserController.java
@@ -3,10 +3,13 @@ package org.websoso.WSSServer.controller;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.OK;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +39,13 @@ public class UserController {
                 .status(OK)
                 .body(nicknameValidation);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody String userId) {
+        String token = userService.login(Long.valueOf(userId));
+        return ResponseEntity
+                .status(OK)
+                .body(Map.of("Authorization", token));
+    }
+
 }
--- src/main/java/org/websoso/WSSServer/service/UserService.java
+++ src/main/java/org/websoso/WSSServer/service/UserService.java
@@ -3,6 +3,9 @@ package org.websoso.WSSServer.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.websoso.WSSServer.config.jwt.JwtProvider;
+import org.websoso.WSSServer.config.jwt.UserAuthentication;
+import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.dto.User.NicknameValidation;
 import org.websoso.WSSServer.repository.UserRepository;
 
@@ -12,10 +15,25 @@ import org.websoso.WSSServer.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
     @Transactional(readOnly = true)
     public NicknameValidation isNicknameAvailable(String nickname) {
         boolean isNicknameTaken = userRepository.existsByNickname(nickname);
         return NicknameValidation.of(isNicknameTaken);
     }
+
+    public String login(Long userId) {
+        User user = getUserOrException(userId);
+
+        UserAuthentication userAuthentication = new UserAuthentication(user.getUserId(), null, null);
+
+        return jwtProvider.generateToken(userAuthentication);
+    }
+
+    public User getUserOrException(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() ->
+                new RuntimeException("유저 없음")); // 수정 예정
+    }
+
 }
