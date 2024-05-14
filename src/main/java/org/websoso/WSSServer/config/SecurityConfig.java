1425de47c1330090131d3a26ae9030963490d569
ChaeAg <dydkglel123@gmail.com>
2024-05-14 11:21:44 +0900

    [REMOVE] SecurityConfig 파일 내 주석 제거

--- src/main/java/org/websoso/WSSServer/config/SecurityConfig.java
+++ src/main/java/org/websoso/WSSServer/config/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.websoso.WSSServer.config.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity //web Security를 사용할 수 있게
+@EnableWebSecurity
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final CustomJwtAuthenticationEntryPoint customJwtAuthenticationEntryPoint;
@@ -31,26 +31,19 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        /*
-        http의 특정 보안 구성을 비활성화하고, 인증 인가에 대한 예외를 처리
-        */
-        http.csrf(AbstractHttpConfigurer::disable) // csrf 공격을 대비하기 위한 csrf 토큰 disable 하기
-                .formLogin(AbstractHttpConfigurer::disable) // form login 비활성화 jwt를 사용하고 있으므로 폼 기반 로그인은 필요하지 않다.
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(
-                        AbstractHttpConfigurer::disable)// http 기본 인증은 사용자 이름과 비밀번호를 평문으로 전송하기 때문에 보안적으로 취약, 기본 인증을 비활성화 하고 있음
+                        AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                }) // session은 사용하지 않으므로 세션 관리를 STATELESS로 설정
+                })
                 .exceptionHandling(exception ->
                 {
-                    exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint); // 인증 에러
-                    exception.accessDeniedHandler(customAccessDeniedHandler); // 인가 에러
+                    exception.authenticationEntryPoint(customJwtAuthenticationEntryPoint);
+                    exception.accessDeniedHandler(customAccessDeniedHandler);
                 });
 
-        /*
-        UsernamePasswordAuthentication 클래스 앞에 jwtAuthenticationFilter를 등록
-        로그인 API 요청은 인증 인가 없이도 요청이 가능하도록 설정
-        */
         http.authorizeHttpRequests(auth -> {
                     auth.requestMatchers(permitAllPaths).permitAll();
                     auth.anyRequest().authenticated();
@@ -60,7 +53,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // cors에러를 대응하기 위한 메소드
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
