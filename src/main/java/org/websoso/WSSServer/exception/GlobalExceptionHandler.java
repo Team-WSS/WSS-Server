9c8f350cfaf07fb22328f064865d14edaa819b35
ChaeAg <dydkglel123@gmail.com>
2024-05-25 15:24:20 +0900

    [FIX] 전역 예외 처리 방식 변경

--- src/main/java/org/websoso/WSSServer/exception/GlobalExceptionHandler.java
+++ src/main/java/org/websoso/WSSServer/exception/GlobalExceptionHandler.java
@@ -1,12 +1,10 @@
 package org.websoso.WSSServer.exception;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.websoso.WSSServer.exception.category.CategoryErrorCode;
 import org.websoso.WSSServer.exception.category.exeption.InvalidCategoryException;
@@ -19,35 +17,40 @@ import org.websoso.WSSServer.exception.user.exception.InvalidUserException;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(InvalidNicknameException.class)
-    public ErrorResult InvalidNicknameExceptionHandler(InvalidNicknameException e) {
+    public ResponseEntity<ErrorResult> InvalidNicknameExceptionHandler(InvalidNicknameException e) {
         log.error("[InvalidNicknameException] exception ", e);
         UserErrorCode userErrorCode = e.getUserErrorCode();
-        return new ErrorResult(userErrorCode.getCode(), userErrorCode.getDescription());
+        return ResponseEntity
+                .status(userErrorCode.getStatusCode())
+                .body(new ErrorResult(userErrorCode.getCode(), userErrorCode.getDescription()));
     }
 
-    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(InvalidUserException.class)
-    public ErrorResult InvalidUserExceptionHandler(InvalidUserException e) {
+    public ResponseEntity<ErrorResult> InvalidUserExceptionHandler(InvalidUserException e) {
         log.error("[InvalidUserException] exception ", e);
         UserErrorCode userErrorCode = e.getUserErrorCode();
-        return new ErrorResult(userErrorCode.getCode(), userErrorCode.getDescription());
+        return ResponseEntity
+                .status(userErrorCode.getStatusCode())
+                .body(new ErrorResult(userErrorCode.getCode(), userErrorCode.getDescription()));
     }
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(InvalidCategoryException.class)
-    public ErrorResult InvalidCategoryExceptionHandler(InvalidCategoryException e) {
+    public ResponseEntity<ErrorResult> InvalidCategoryExceptionHandler(InvalidCategoryException e) {
         log.error("[InvalidCategoryException] exception ", e);
         CategoryErrorCode categoryErrorCode = e.getCategoryErrorCode();
-        return new ErrorResult(categoryErrorCode.getCode(), categoryErrorCode.getDescription());
+        return ResponseEntity
+                .status(categoryErrorCode.getStatusCode())
+                .body(new ErrorResult(categoryErrorCode.getCode(), categoryErrorCode.getDescription()));
     }
 
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResult InvalidFeedExceptionHandler(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResult> InvalidFeedExceptionHandler(MethodArgumentNotValidException e) {
         log.error("[MethodArgumentNotValidException] exception ", e);
-        return new ErrorResult(BAD_REQUEST.getReasonPhrase(),
-                e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        HttpStatus httpStatus = HttpStatus.valueOf(e.getStatusCode().value());
+        return ResponseEntity.status(httpStatus)
+                .body(new ErrorResult(httpStatus.name(),
+                        e.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
     }
+
 }
