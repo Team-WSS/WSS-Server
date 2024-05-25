6ed8ad4bdf22fe305fc62bbacf95ef43db389083
ChaeAg <dydkglel123@gmail.com>
2024-05-25 15:28:08 +0900

    [STYLE] 코드 내 불필요한 콤마(,) 제거

--- src/main/java/org/websoso/WSSServer/exception/user/UserErrorCode.java
+++ src/main/java/org/websoso/WSSServer/exception/user/UserErrorCode.java
@@ -17,8 +17,7 @@ public enum UserErrorCode implements IErrorCode {
     INVALID_NICKNAME_START_OR_END_WITH_BLANK("USER-003", "닉네임은 공백으로 시작하거나 끝날 수 없습니다.", BAD_REQUEST),
     INVALID_NICKNAME_LENGTH("USER-004", "닉네임의 길이가 2 ~ 10자가 아닙니다.", BAD_REQUEST),
     INVALID_NICKNAME_PATTERN("USER-005", "닉네임은 한글, 영문, 숫자, 특수문자(-, _)로만 이루어져아 합니다.", BAD_REQUEST),
-    USER_NOT_FOUND("USER-006", "해당 ID를 가진 사용자를 찾을 수 없습니다.", NOT_FOUND),
-    ;
+    USER_NOT_FOUND("USER-006", "해당 ID를 가진 사용자를 찾을 수 없습니다.", NOT_FOUND);
 
     private final String code;
     private final String description;
