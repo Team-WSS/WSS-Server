bb39c4ce3384e420412a1a996338cf1baf999a99
nakyeong <rinarina0429@naver.com>
2024-05-27 23:21:40 +0900

    [FIX] Genre 도메인 추가

---
+++ src/main/java/org/websoso/WSSServer/domain/Genre.java
@@ -0,0 +1,26 @@
+package org.websoso.WSSServer.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long genreId;
+
+    @Column(columnDefinition = "varchar(5)", nullable = false)
+    private String genreName;
+
+    @Column(columnDefinition = "text", nullable = false)
+    private String genreBadge;
+}
