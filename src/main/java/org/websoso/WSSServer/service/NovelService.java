9fe8bfebb35b2c773049755b719b1170818604e9
nakyeong <rinarina0429@naver.com>
2024-06-04 14:53:17 +0900

    [REFACTOR] 에러 코드 static import로 변경

--- src/main/java/org/websoso/WSSServer/service/NovelService.java
+++ src/main/java/org/websoso/WSSServer/service/NovelService.java
@@ -1,5 +1,8 @@
 package org.websoso.WSSServer.service;
 
+import static org.websoso.WSSServer.exception.novel.NovelErrorCode.NOVEL_NOT_FOUND;
+import static org.websoso.WSSServer.exception.novelStatistics.NovelStatisticsErrorCode.NOVEL_STATISTICS_NOT_FOUND;
+
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -11,9 +14,7 @@ import org.websoso.WSSServer.domain.NovelStatistics;
 import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.domain.UserNovel;
 import org.websoso.WSSServer.dto.novel.NovelGetResponse1;
-import org.websoso.WSSServer.exception.novel.NovelErrorCode;
 import org.websoso.WSSServer.exception.novel.exception.InvalidNovelException;
-import org.websoso.WSSServer.exception.novelStatistics.NovelStatisticsErrorCode;
 import org.websoso.WSSServer.exception.novelStatistics.exception.InvalidNovelStatisticsException;
 import org.websoso.WSSServer.repository.NovelRepository;
 import org.websoso.WSSServer.repository.NovelStatisticsRepository;
@@ -39,14 +40,14 @@ public class NovelService {
     }
 
     private Novel getNovelOrException(Long novelId) {
-        return novelRepository.findById(novelId).orElseThrow(
-                () -> new InvalidNovelException(NovelErrorCode.NOVEL_NOT_FOUND,
+        return novelRepository.findById(novelId)
+                .orElseThrow(() -> new InvalidNovelException(NOVEL_NOT_FOUND,
                         "novel with the given id is not found"));
     }
 
     private NovelStatistics getNovelStatisticsOrException(Novel novel) {
         return novelStatisticsRepository.findByNovel(novel).orElseThrow(
-                () -> new InvalidNovelStatisticsException(NovelStatisticsErrorCode.NOVEL_STATISTICS_NOT_FOUND,
+                () -> new InvalidNovelStatisticsException(NOVEL_STATISTICS_NOT_FOUND,
                         "novel statistics with the given novel is not found"));
     }
 
