ff85f98e1295b4127ff95ec761bef04badf567d5
nakyeong <rinarina0429@naver.com>
2024-06-04 17:49:32 +0900

    [RENAME] NovelStatistics 에러코드 표기 수정

--- src/main/java/org/websoso/WSSServer/exception/novelStatistics/NovelStatisticsErrorCode.java
+++ src/main/java/org/websoso/WSSServer/exception/novelStatistics/NovelStatisticsErrorCode.java
@@ -9,7 +9,7 @@ import org.websoso.WSSServer.exception.common.IErrorCode;
 @AllArgsConstructor
 public enum NovelStatisticsErrorCode implements IErrorCode {
 
-    NOVEL_STATISTICS_NOT_FOUND("NOVELSTATISTICS-001", "해당 작품의 통계를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    NOVEL_STATISTICS_NOT_FOUND("NOVEL_STATISTICS-001", "해당 작품의 통계를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String description;
