3d57464ca8d8d85ad4d94f7206e0f7f6c4c7e615
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 05:50:47 +0900

    [FEAT] NOTICE_NOT_FOUND 에러코드 추가

--- src/main/java/org/websoso/WSSServer/exception/notice/NoticeErrorCode.java
+++ src/main/java/org/websoso/WSSServer/exception/notice/NoticeErrorCode.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.exception.notice;
 
 import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +12,8 @@ import org.websoso.WSSServer.exception.common.IErrorCode;
 @Getter
 public enum NoticeErrorCode implements IErrorCode {
 
-    NOTICE_FORBIDDEN("NOTICE-001", "관리자가 아닌 계정은 공지사항을 작성할 수 없습니다.", FORBIDDEN);
+    NOTICE_FORBIDDEN("NOTICE-001", "관리자가 아닌 계정은 공지사항을 작성 혹은 수정할 수 없습니다.", FORBIDDEN),
+    NOTICE_NOT_FOUND("NOTICE-002", "해당 ID를 가진 공지사항을 찾을 수 없습니다.", NOT_FOUND);
 
     private final String code;
     private final String description;
