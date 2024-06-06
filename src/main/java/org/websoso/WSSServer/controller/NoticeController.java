ed8a561a71d1f81f00f6ce2b7bf6ea20c49dad79
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 02:17:42 +0900

    [FEAT] 공지사항 생성 컨트롤러에서 토큰 정보 받아오는 로직 추가

--- src/main/java/org/websoso/WSSServer/controller/NoticeController.java
+++ src/main/java/org/websoso/WSSServer/controller/NoticeController.java
@@ -2,14 +2,17 @@ package org.websoso.WSSServer.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.dto.notice.NoticePostRequest;
 import org.websoso.WSSServer.service.NoticeService;
+import org.websoso.WSSServer.service.UserService;
 
 @RestController
 @RequestMapping("/notices")
@@ -17,10 +20,13 @@ import org.websoso.WSSServer.service.NoticeService;
 public class NoticeController {
 
     private final NoticeService noticeService;
+    private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<Void> createNotice(@RequestBody NoticePostRequest noticePostRequest) {
-        noticeService.createNotice(noticePostRequest);
+    public ResponseEntity<Void> createNotice(Principal principal,
+                                             @RequestBody NoticePostRequest noticePostRequest) {
+        User user = userService.getUserOrException(Long.valueOf(principal.getName()));
+        noticeService.createNotice(user, noticePostRequest);
         return ResponseEntity
                 .status(CREATED)
                 .build();
