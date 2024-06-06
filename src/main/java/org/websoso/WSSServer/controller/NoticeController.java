ecdb152860f303bcdbd1edaf4ce675ef7ebf268d
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 05:47:32 +0900

    [FEAT] 공지사항 수정을 위한 엔드포인트 추가

--- src/main/java/org/websoso/WSSServer/controller/NoticeController.java
+++ src/main/java/org/websoso/WSSServer/controller/NoticeController.java
@@ -1,16 +1,20 @@
 package org.websoso.WSSServer.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 import jakarta.validation.Valid;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.dto.notice.NoticeEditRequest;
 import org.websoso.WSSServer.dto.notice.NoticePostRequest;
 import org.websoso.WSSServer.service.NoticeService;
 import org.websoso.WSSServer.service.UserService;
@@ -30,6 +34,17 @@ public class NoticeController {
         noticeService.createNotice(user, noticePostRequest);
         return ResponseEntity
                 .status(CREATED)
+                .build();
+    }
+
+    @PutMapping("/{noticeId}")
+    public ResponseEntity<Void> editNotice(Principal principal,
+                                           @PathVariable("noticeId") Long noticeId,
+                                           @Valid @RequestBody NoticeEditRequest noticeEditRequest) {
+        User user = userService.getUserOrException(Long.valueOf(principal.getName()));
+        noticeService.editNotice(user, noticeId, noticeEditRequest);
+        return ResponseEntity
+                .status(NO_CONTENT)
                 .build();
     }
 }
