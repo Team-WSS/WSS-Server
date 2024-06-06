05a5173990172bbc66888b119fc400739b168c0c
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 06:37:44 +0900

    [FEAT] 공지사항 조회 JPA 쿼리 메서드 추가

--- src/main/java/org/websoso/WSSServer/repository/NoticeRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/NoticeRepository.java
@@ -1,5 +1,6 @@
 package org.websoso.WSSServer.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Notice;
@@ -7,4 +8,5 @@ import org.websoso.WSSServer.domain.Notice;
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
+    List<Notice> findByUserId(Long userId);
 }
