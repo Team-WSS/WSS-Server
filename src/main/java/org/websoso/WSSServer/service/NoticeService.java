8d90e0a711e3708acad4967fd305a1324155bf32
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 05:49:47 +0900

    [FEAT] 공지사항 수정을 위한 서비스 로직 추가

--- src/main/java/org/websoso/WSSServer/service/NoticeService.java
+++ src/main/java/org/websoso/WSSServer/service/NoticeService.java
@@ -2,6 +2,7 @@ package org.websoso.WSSServer.service;
 
 import static org.websoso.WSSServer.domain.common.Role.ADMIN;
 import static org.websoso.WSSServer.exception.notice.NoticeErrorCode.NOTICE_FORBIDDEN;
+import static org.websoso.WSSServer.exception.notice.NoticeErrorCode.NOTICE_NOT_FOUND;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.websoso.WSSServer.domain.Notice;
 import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.domain.common.Role;
+import org.websoso.WSSServer.dto.notice.NoticeEditRequest;
 import org.websoso.WSSServer.dto.notice.NoticePostRequest;
 import org.websoso.WSSServer.exception.notice.exception.ForbiddenNoticeCreationException;
+import org.websoso.WSSServer.exception.notice.exception.NoticeNotFoundException;
 import org.websoso.WSSServer.repository.NoticeRepository;
 
 @Service
@@ -31,9 +34,22 @@ public class NoticeService {
                 .build());
     }
 
+    @Transactional
+    public void editNotice(User user, Long noticeId, NoticeEditRequest noticeEditRequest) {
+        validateAuthorization(user);
+        Notice notice = getNoticeOrException(noticeId);
+        notice.updateNotice(noticeEditRequest.noticeTitle(), noticeEditRequest.noticeContent(),
+                noticeEditRequest.userId());
+    }
+
     private static void validateAuthorization(User user) {
         if (user.getRole() != ADMIN_ROLE) {
             throw new ForbiddenNoticeCreationException(NOTICE_FORBIDDEN, "user is not ADMIN");
         }
+    }
+
+    private Notice getNoticeOrException(Long noticeId) {
+        return noticeRepository.findById(noticeId).orElseThrow(() ->
+                new NoticeNotFoundException(NOTICE_NOT_FOUND, "notice with given noticeId was not found"));
     }
 }
