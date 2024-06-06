9c302c8b9c0a889e27b535350b552d27c73f3fc9
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 06:38:24 +0900

    [FIX] 전체 유저 대상 공지 누락 수정

--- src/main/java/org/websoso/WSSServer/repository/NoticeRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/NoticeRepository.java
@@ -2,11 +2,13 @@ package org.websoso.WSSServer.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Notice;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
+    @Query(value = "SELECT n FROM Notice n WHERE n.userId = ?1 OR n.userId = 0")
     List<Notice> findByUserId(Long userId);
 }
