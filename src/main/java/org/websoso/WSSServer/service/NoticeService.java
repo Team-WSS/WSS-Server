1da946d1fde4f201fe01991672ba65547834a37c
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 02:18:22 +0900

    [FEAT] 서비스에서 공지사항 생성 시 권한 체크 로직 추가

--- src/main/java/org/websoso/WSSServer/service/NoticeService.java
+++ src/main/java/org/websoso/WSSServer/service/NoticeService.java
@@ -1,10 +1,16 @@
 package org.websoso.WSSServer.service;
 
+import static org.websoso.WSSServer.domain.common.Role.ADMIN;
+import static org.websoso.WSSServer.exception.notice.NoticeErrorCode.NOTICE_FORBIDDEN;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.websoso.WSSServer.domain.Notice;
+import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.domain.common.Role;
 import org.websoso.WSSServer.dto.notice.NoticePostRequest;
+import org.websoso.WSSServer.exception.notice.exception.ForbiddenNoticeCreationException;
 import org.websoso.WSSServer.repository.NoticeRepository;
 
 @Service
@@ -13,9 +19,13 @@ import org.websoso.WSSServer.repository.NoticeRepository;
 public class NoticeService {
 
     private final NoticeRepository noticeRepository;
+    private static final Role ADMIN_ROLE = ADMIN;
 
     @Transactional
-    public void createNotice(NoticePostRequest noticePostRequest) {
+    public void createNotice(User user, NoticePostRequest noticePostRequest) {
+        if (user.getRole() != ADMIN_ROLE) {
+            throw new ForbiddenNoticeCreationException(NOTICE_FORBIDDEN, "user is not ADMIN");
+        }
         noticeRepository.save(Notice.builder()
                 .noticeTitle(noticePostRequest.noticeTitle())
                 .noticeContent(noticePostRequest.noticeContent())
