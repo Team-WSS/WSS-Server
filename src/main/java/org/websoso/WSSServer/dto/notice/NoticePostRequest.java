dc0fccdce70a0aed3b913a2a063e1d22586288f6
Kim-TaeUk <xodnr0130@naver.com>
2024-06-07 04:33:16 +0900

    [FEAT] 공지 제목, 내용 validation 어노테이션 추가

--- src/main/java/org/websoso/WSSServer/dto/notice/NoticePostRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/notice/NoticePostRequest.java
@@ -1,8 +1,18 @@
 package org.websoso.WSSServer.dto.notice;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record NoticePostRequest(
+
+        @NotBlank(message = "공지 제목은 비어 있거나, 공백일 수 없습니다.")
+        @Size(max = 200, message = "공지 제목은 200자를 초과할 수 없습니다.")
         String noticeTitle,
+
+        @NotBlank(message = "공지 내용은 비어 있거나, 공백일 수 없습니다.")
+        @Size(max = 2000, message = "공지 내용은 2000자를 초과할 수 없습니다.")
         String noticeContent,
+
         Long userId
 ) {
 }
