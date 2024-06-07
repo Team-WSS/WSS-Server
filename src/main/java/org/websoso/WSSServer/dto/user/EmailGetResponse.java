1655f6fdff0d8390e23a417c9bbf27de47b793b7
Kim-TaeUk <xodnr0130@naver.com>
2024-06-08 02:08:07 +0900

    [FEAT] 유저 이메일 조회 결과 반환할 DTO 추가

---
+++ src/main/java/org/websoso/WSSServer/dto/user/EmailGetResponse.java
@@ -0,0 +1,10 @@
+package org.websoso.WSSServer.dto.user;
+
+public record EmailGetResponse(
+        String email
+) {
+
+    public static EmailGetResponse of(String email) {
+        return new EmailGetResponse(email);
+    }
+}
