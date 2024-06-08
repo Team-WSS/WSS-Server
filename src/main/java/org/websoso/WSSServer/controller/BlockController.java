445ca9e9f40ceab6abdb9264829d491d1324dcfc
Kim-TaeUk <xodnr0130@naver.com>
2024-06-08 23:53:45 +0900

    [FIX] 유저 차단 엔드포인트 변경
    /users/blocks -> /blocks

---
+++ src/main/java/org/websoso/WSSServer/controller/BlockController.java
@@ -0,0 +1,34 @@
+package org.websoso.WSSServer.controller;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.service.BlockService;
+import org.websoso.WSSServer.service.UserService;
+
+@RestController
+@RequestMapping("/blocks")
+@RequiredArgsConstructor
+public class BlockController {
+
+    private final UserService userService;
+    private final BlockService blockService;
+
+    @PostMapping
+    public ResponseEntity<Void> block(Principal principal,
+                                      @RequestParam("userId") Long blockedId) {
+        System.out.println("---------------kingking");
+        User blocker = userService.getUserOrException(Long.valueOf(principal.getName()));
+        blockService.block(blocker, blockedId);
+        return ResponseEntity
+                .status(CREATED)
+                .build();
+    }
+}
