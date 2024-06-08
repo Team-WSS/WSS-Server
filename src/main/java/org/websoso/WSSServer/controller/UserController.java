eb4a59811fe02058ab9726352cc14a524891b73d
Kim-TaeUk <xodnr0130@naver.com>
2024-06-08 23:45:18 +0900

    [FEAT] 유저 차단을 위한 엔드포인트 추가

--- src/main/java/org/websoso/WSSServer/controller/UserController.java
+++ src/main/java/org/websoso/WSSServer/controller/UserController.java
@@ -1,8 +1,10 @@
 package org.websoso.WSSServer.controller;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.dto.User.LoginResponse;
 import org.websoso.WSSServer.dto.User.NicknameValidation;
+import org.websoso.WSSServer.service.BlockService;
 import org.websoso.WSSServer.service.UserService;
 import org.websoso.WSSServer.validation.NicknameConstraint;
 
@@ -24,6 +28,7 @@ import org.websoso.WSSServer.validation.NicknameConstraint;
 public class UserController {
 
     private final UserService userService;
+    private final BlockService blockService;
 
     @GetMapping("/nickname/check")
     public ResponseEntity<NicknameValidation> checkNicknameAvailability(
@@ -46,5 +51,15 @@ public class UserController {
         return ResponseEntity
                 .status(OK)
                 .body(response);
+    }
+
+    @PostMapping("/blocks")
+    public ResponseEntity<Void> block(Principal principal,
+                                      @RequestParam("userId") Long blockedId) {
+        User blocker = userService.getUserOrException(Long.valueOf(principal.getName()));
+        blockService.block(blocker, blockedId);
+        return ResponseEntity
+                .status(CREATED)
+                .build();
     }
 }
