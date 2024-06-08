7266e9bb367faf3ac781e7178c8e9a400da0087f
Kim-TaeUk <xodnr0130@naver.com>
2024-06-09 05:31:08 +0900

    [FEAT] 차단 취소 엔드포인트 추가

--- src/main/java/org/websoso/WSSServer/controller/BlockController.java
+++ src/main/java/org/websoso/WSSServer/controller/BlockController.java
@@ -1,12 +1,15 @@
 package org.websoso.WSSServer.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,5 +44,15 @@ public class BlockController {
         return ResponseEntity
                 .status(OK)
                 .body(blockService.getBlockList(user));
+    }
+
+    @DeleteMapping("/{blockId}")
+    public ResponseEntity<Void> deleteBlock(Principal principal,
+                                            @PathVariable("blockId") Long blockId) {
+        User user = userService.getUserOrException(Long.valueOf(principal.getName()));
+        blockService.deleteBlock(user, blockId);
+        return ResponseEntity
+                .status(NO_CONTENT)
+                .build();
     }
 }
