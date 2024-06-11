5a29533c0088df81dbc6f2c2ba1902236a26b5ff
Kim-TaeUk <xodnr0130@naver.com>
2024-06-11 19:19:36 +0900

    [REMOVE] 불필요한 개행 제거

--- src/main/java/org/websoso/WSSServer/exception/avatar/exception/AvatarNotFoundException.java
+++ src/main/java/org/websoso/WSSServer/exception/avatar/exception/AvatarNotFoundException.java
@@ -6,7 +6,6 @@ import org.websoso.WSSServer.exception.avatar.AvatarErrorCode;
 
 @Getter
 @AllArgsConstructor
-
 public class AvatarNotFoundException extends RuntimeException {
 
     public AvatarNotFoundException(AvatarErrorCode avatarErrorCode, String message) {
