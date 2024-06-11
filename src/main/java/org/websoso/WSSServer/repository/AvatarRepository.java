5d678ca829e9894a2821e10248b45ca1360d66b3
ChaeAg <dydkglel123@gmail.com>
2024-06-11 16:01:12 +0900

    [REMOVE] 리베이스 시 제거 안된 코드 제거

--- src/main/java/org/websoso/WSSServer/repository/AvatarRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/AvatarRepository.java
@@ -6,8 +6,4 @@ import org.websoso.WSSServer.domain.Avatar;
 
 @Repository
 public interface AvatarRepository extends JpaRepository<Avatar, Byte> {
-<<<<<<< HEAD
-
-=======
->>>>>>> 8e88fcb ([FEAT] 소소피드 단건 조회 API 구현)
 }
