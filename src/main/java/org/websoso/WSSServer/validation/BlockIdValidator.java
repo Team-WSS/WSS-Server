c0adcdf04e9c81c959fa1f892fb364d7f7ff0c6c
Kim-TaeUk <xodnr0130@naver.com>
2024-06-13 15:05:09 +0900

    [FIX] BlockIdValidator 기능 수정
    유효한 값 검증만 하도록 변경

--- src/main/java/org/websoso/WSSServer/validation/BlockIdValidator.java
+++ src/main/java/org/websoso/WSSServer/validation/BlockIdValidator.java
@@ -1,21 +1,16 @@
 package org.websoso.WSSServer.validation;
 
-import static org.websoso.WSSServer.exception.block.BlockErrorCode.BLOCK_NOT_FOUND;
 import static org.websoso.WSSServer.exception.block.BlockErrorCode.INVALID_BLOCK_ID;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.websoso.WSSServer.exception.block.exception.BlockNotFoundException;
 import org.websoso.WSSServer.exception.block.exception.InvalidBlockIdException;
-import org.websoso.WSSServer.repository.BlockRepository;
 
 @Component
 @AllArgsConstructor
 public class BlockIdValidator implements ConstraintValidator<BlockIdConstraint, Long> {
-
-    private final BlockRepository blockRepository;
 
     @Override
     public void initialize(BlockIdConstraint blockId) {
@@ -26,8 +21,6 @@ public class BlockIdValidator implements ConstraintValidator<BlockIdConstraint, 
         if (blockId <= 0) {
             throw new InvalidBlockIdException(INVALID_BLOCK_ID, "given blockId is an invalid value");
         }
-        blockRepository.findById(blockId).orElseThrow(() ->
-                new BlockNotFoundException(BLOCK_NOT_FOUND, "block with the given blockId was not found"));
 
         return true;
     }
