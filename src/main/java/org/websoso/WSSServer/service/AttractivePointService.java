a292a47a262279c0c879e9bf0e634b0c922d0a17
nakyeong <rinarina0429@naver.com>
2024-06-15 22:43:09 +0900

    [REFACTOR] 매력포인트 서비스 내 예외처리 구체화

--- src/main/java/org/websoso/WSSServer/service/AttractivePointService.java
+++ src/main/java/org/websoso/WSSServer/service/AttractivePointService.java
@@ -1,11 +1,14 @@
 package org.websoso.WSSServer.service;
 
+import static org.websoso.WSSServer.exception.attractivePoint.AttractivePointErrorCode.INVALID_ATTRACTIVE_POINT;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.websoso.WSSServer.domain.AttractivePoint;
 import org.websoso.WSSServer.domain.UserNovel;
 import org.websoso.WSSServer.domain.common.Flag;
+import org.websoso.WSSServer.exception.attractivePoint.exception.InvalidAttractivePointException;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +24,8 @@ public class AttractivePointService {
                 case "material" -> attractivePoint.setMaterial(Flag.Y);
                 case "character" -> attractivePoint.setCharacters(Flag.Y);
                 case "relationship" -> attractivePoint.setRelationship(Flag.Y);
-                default -> throw new IllegalArgumentException("Invalid attractive point: " + point);
+                default -> throw new InvalidAttractivePointException(INVALID_ATTRACTIVE_POINT,
+                        "invalid attractive point provided in the request");
             }
         }
         return attractivePoint;
