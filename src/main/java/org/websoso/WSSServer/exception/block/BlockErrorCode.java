024c0bd25047d8f22ee54a09a68a8c9232a94632
Kim-TaeUk <xodnr0130@naver.com>
2024-06-16 18:06:44 +0900

    [FEAT] 커스텀 예외와 에러코드 정의

--- src/main/java/org/websoso/WSSServer/exception/block/BlockErrorCode.java
+++ src/main/java/org/websoso/WSSServer/exception/block/BlockErrorCode.java
@@ -18,7 +18,8 @@ public enum BlockErrorCode implements IErrorCode {
     SELF_BLOCKED("BLOCK-002", "본인을 차단할 수 없습니다.", BAD_REQUEST),
     BLOCK_NOT_FOUND("BLOCK-003", "해당 ID에 해당하는 차단을 찾을 수 없습니다.", NOT_FOUND),
     INVALID_BLOCK_ID("BLOCK-004", "유효하지 않은 ID입니다.", BAD_REQUEST),
-    INVALID_AUTHORIZED_BLOCK("BLOCK-005", "해당 유저의 차단이 아닙니다.", FORBIDDEN);
+    INVALID_AUTHORIZED_BLOCK("BLOCK-005", "해당 유저의 차단이 아닙니다.", FORBIDDEN),
+    CANNOT_ADMIN_BLOCK("BLOCK-006", "관리자는 차단할 수 없습니다.", FORBIDDEN);
 
     private final String code;
     private final String description;
