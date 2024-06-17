8bb3e8d0c0f81e6ef99887f97cfb780570226cdd
nakyeong <rinarina0429@naver.com>
2024-06-18 01:50:59 +0900

    [FEAT] KeywordCountGetResponse에 @Getter 추가

--- src/main/java/org/websoso/WSSServer/dto/Keyword/KeywordCountGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/Keyword/KeywordCountGetResponse.java
@@ -1,7 +1,9 @@
 package org.websoso.WSSServer.dto.Keyword;
 
+import lombok.Getter;
 import org.websoso.WSSServer.domain.Keyword;
 
+@Getter
 public record KeywordCountGetResponse(
         String keywordName,
         Integer keywordCount
