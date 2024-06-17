728dd09be248f1adbc74195a8f5b91d990ddefac
nakyeong <rinarina0429@naver.com>
2024-06-18 02:08:56 +0900

    [FEAT] 키워드 관련 예외 처리 추가

--- src/main/java/org/websoso/WSSServer/exception/keyword/KeywordErrorCode.java
+++ src/main/java/org/websoso/WSSServer/exception/keyword/KeywordErrorCode.java
@@ -1,4 +1,20 @@
 package org.websoso.WSSServer.exception.keyword;
 
-public enum KeywordErrorCode {
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.websoso.WSSServer.exception.common.IErrorCode;
+
+@AllArgsConstructor
+@Getter
+public enum KeywordErrorCode implements IErrorCode {
+
+    KEYWORD_NOT_FOUND("KEYWORD-001", "해당 ID를 가진 키워드를 찾을 수 없습니다.", NOT_FOUND);
+
+    private final String code;
+    private final String description;
+    private final HttpStatus statusCode;
+
 }
--- src/main/java/org/websoso/WSSServer/exception/keyword/exception/InvalidKeywordException.java
+++ src/main/java/org/websoso/WSSServer/exception/keyword/exception/InvalidKeywordException.java
@@ -1,4 +1,17 @@
 package org.websoso.WSSServer.exception.keyword.exception;
 
-public class InvalidKeywordException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.websoso.WSSServer.exception.keyword.KeywordErrorCode;
+
+@Getter
+@AllArgsConstructor
+public class InvalidKeywordException extends RuntimeException {
+
+    public InvalidKeywordException(KeywordErrorCode keywordErrorCode, String message) {
+        super(message);
+        this.keywordErrorCode = keywordErrorCode;
+    }
+
+    private KeywordErrorCode keywordErrorCode;
 }
