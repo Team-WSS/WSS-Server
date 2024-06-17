45377be0b22949b74a231813aa12c973ea72928f
nakyeong <rinarina0429@naver.com>
2024-06-18 02:41:18 +0900

    [FIX] dto에서 @Getter 어노테이션 제거

--- src/main/java/org/websoso/WSSServer/dto/Keyword/KeywordCountGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/Keyword/KeywordCountGetResponse.java
@@ -1,9 +1,7 @@
 package org.websoso.WSSServer.dto.Keyword;
 
-import lombok.Getter;
 import org.websoso.WSSServer.domain.Keyword;
 
-@Getter
 public record KeywordCountGetResponse(
         String keywordName,
         Integer keywordCount
