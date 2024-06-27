d7eaa655c8580d4d81fd6cf081efd4dbfb33e0ba
nakyeong <rinarina0429@naver.com>
2024-06-28 00:50:07 +0900

    [REFACTOR] AttractivePointService에서 Flag값이 아닌 Boolean 값 넣도록 수정

--- src/main/java/org/websoso/WSSServer/service/AttractivePointService.java
+++ src/main/java/org/websoso/WSSServer/service/AttractivePointService.java
@@ -1,11 +1,9 @@
 package org.websoso.WSSServer.service;
 
-import static org.websoso.WSSServer.domain.common.Flag.Y;
 import static org.websoso.WSSServer.exception.attractivePoint.AttractivePointErrorCode.INVALID_ATTRACTIVE_POINT;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.DynamicInsert;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.websoso.WSSServer.domain.AttractivePoint;
@@ -19,11 +17,11 @@ public class AttractivePointService {
     public static void setAttractivePoint(AttractivePoint attractivePoint, List<String> request) {
         for (String point : request) {
             switch (point.toLowerCase()) {
-                case "universe" -> attractivePoint.setUniverse(Y);
-                case "vibe" -> attractivePoint.setVibe(Y);
-                case "material" -> attractivePoint.setMaterial(Y);
-                case "character" -> attractivePoint.setCharacters(Y);
-                case "relationship" -> attractivePoint.setRelationship(Y);
+                case "universe" -> attractivePoint.setUniverse(true);
+                case "vibe" -> attractivePoint.setVibe(true);
+                case "material" -> attractivePoint.setMaterial(true);
+                case "character" -> attractivePoint.setCharacters(true);
+                case "relationship" -> attractivePoint.setRelationship(true);
                 default -> throw new InvalidAttractivePointException(INVALID_ATTRACTIVE_POINT,
                         "invalid attractive point provided in the request");
             }
