372b458e18e732b7ffae5f682afc707e9831e34b
ChaeAg <dydkglel123@gmail.com>
2024-06-28 14:45:43 +0900

    [FIX] DTO 검증 수치 오류 수정

--- src/main/java/org/websoso/WSSServer/dto/comment/CommentUpdateRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/comment/CommentUpdateRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record CommentUpdateRequest(
         @NotBlank(message = "댓글 내용은 비어 있거나, 공백일 수 없습니다.")
-        @Size(max = 2000, message = "댓글 내용은 100자를 초과할 수 없습니다.")
+        @Size(max = 100, message = "댓글 내용은 100자를 초과할 수 없습니다.")
         String commentContent
 ) {
 }
