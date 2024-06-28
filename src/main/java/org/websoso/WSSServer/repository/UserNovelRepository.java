1b35ed525e9090eaaede8f8f923edb4392772af9
nakyeong <rinarina0429@naver.com>
2024-06-28 18:25:56 +0900

    [FEAT] UserNovelRepository에 최근 등록된 작품들 순서대로 찾는 메서드 정의

--- src/main/java/org/websoso/WSSServer/repository/UserNovelRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/UserNovelRepository.java
@@ -1,6 +1,8 @@
 package org.websoso.WSSServer.repository;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Novel;
@@ -9,6 +11,9 @@ import org.websoso.WSSServer.domain.UserNovel;
 
 @Repository
 public interface UserNovelRepository extends JpaRepository<UserNovel, Long> {
+
     Optional<UserNovel> findByNovelAndUser(Novel novel, User user);
+
+    Page<UserNovel> findByOrderByCreatedDateDesc(Pageable pageable);
 
 }
