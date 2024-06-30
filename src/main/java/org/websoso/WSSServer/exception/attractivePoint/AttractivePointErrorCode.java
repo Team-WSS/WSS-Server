53d6fc5eab4f8e19979a7e903b449be96b105316
nakyeong <rinarina0429@naver.com>
2024-07-01 00:03:44 +0900

    [FEAT] 매력포인트 못 찾을 시 에러코드 추가

--- src/main/java/org/websoso/WSSServer/exception/attractivePoint/AttractivePointErrorCode.java
+++ src/main/java/org/websoso/WSSServer/exception/attractivePoint/AttractivePointErrorCode.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.exception.attractivePoint;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +12,8 @@ import org.websoso.WSSServer.exception.common.IErrorCode;
 @AllArgsConstructor
 public enum AttractivePointErrorCode implements IErrorCode {
 
-    INVALID_ATTRACTIVE_POINT("ATTRACTIVE_POINT-001", "해당 매력포인트는 존재하지 않습니다.", BAD_REQUEST);
+    ATTRACTIVE_POINT_NOT_FOUND("ATTRACTIVE_POINT-001", "해당 서재 작품의 매력포인트를 찾을 수 없습니다.", NOT_FOUND),
+    INVALID_ATTRACTIVE_POINT("ATTRACTIVE_POINT-002", "해당 매력포인트는 존재하지 않습니다.", BAD_REQUEST);
 
     private final String code;
     private final String description;
