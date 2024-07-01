1f5b7750934f3554269e1562539cb23c7bc7761f
ChaeAg <dydkglel123@gmail.com>
2024-07-01 17:20:58 +0900

    [FEAT] 두 유저 간 차단관계 확인 로직 QueryDsl 사용하여 성능 개선

--- src/main/java/org/websoso/WSSServer/repository/BlockRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/BlockRepository.java
@@ -2,19 +2,13 @@ package org.websoso.WSSServer.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Block;
 
 @Repository
-public interface BlockRepository extends JpaRepository<Block, Long> {
+public interface BlockRepository extends JpaRepository<Block, Long>, BlockCustomRepository {
 
     boolean existsByBlockingIdAndBlockedId(Long blockingId, Long blockedId);
 
     List<Block> findByBlockingId(Long blockingId);
-
-    @Query("SELECT (COUNT(b) > 0) FROM Block b WHERE (b.blockingId = :firstUserId AND b.blockedId = :secondUserId) OR (b.blockingId = :secondUserId AND b.blockedId = :firstUserId)")
-    boolean existsByTwoUserId(@Param("firstUserId") Long firstUserId, @Param("secondUserId") Long secondUserId);
-
 }
