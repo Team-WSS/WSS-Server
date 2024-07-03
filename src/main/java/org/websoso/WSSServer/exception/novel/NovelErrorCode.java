381c70a9ce019a21d7735fce30c25dd41831f23c
nakyeong <rinarina0429@naver.com>
2024-07-04 01:12:08 +0900

    [REFACTOR] ALREADY_INTERESTED 에러 설명 수정

--- src/main/java/org/websoso/WSSServer/exception/novel/NovelErrorCode.java
+++ src/main/java/org/websoso/WSSServer/exception/novel/NovelErrorCode.java
@@ -13,7 +13,7 @@ import org.websoso.WSSServer.exception.common.IErrorCode;
 public enum NovelErrorCode implements IErrorCode {
 
     NOVEL_NOT_FOUND("NOVEL-001", "해당 ID를 가진 작품을 찾을 수 없습니다.", NOT_FOUND),
-    ALREADY_INTERESTED("NOVEL-002", "이미 해당 작품이 관심있어요에 등록되어 있습니다.", CONFLICT);
+    ALREADY_INTERESTED("NOVEL-002", "이미 해당 작품이 관심 작품으로 등록되어 있습니다.", CONFLICT);
 
     private final String code;
     private final String description;
