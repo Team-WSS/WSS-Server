d3f0a596d79934cf6e1dcdb69ecec652296fc82f
nakyeong <rinarina0429@naver.com>
2024-07-05 17:17:36 +0900

    [REFACTOR] 쿼리문 개행 추가

--- src/main/java/org/websoso/WSSServer/repository/NovelRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/NovelRepository.java
@@ -9,6 +9,9 @@ import org.websoso.WSSServer.domain.Novel;
 @Repository
 public interface NovelRepository extends JpaRepository<Novel, Long> {
 
-    @Query(value = "SELECT n.* FROM novel n JOIN (SELECT un.novel_id FROM user_novel un GROUP BY un.novel_id ORDER BY MAX(un.created_date) DESC LIMIT 10) latest_un ON n.novel_id = latest_un.novel_id", nativeQuery = true)
+    @Query(value = "SELECT n.* FROM novel n " +
+            "JOIN (SELECT un.novel_id FROM user_novel un " +
+            "GROUP BY un.novel_id ORDER BY MAX(un.created_date) DESC LIMIT 10) latest_un " +
+            "ON n.novel_id = latest_un.novel_id", nativeQuery = true)
     List<Novel> findSosoPick();
 }
