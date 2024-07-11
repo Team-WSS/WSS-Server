21d55dbef570dc646f8f9f1cb54e097b75704d09
ChaeAg <dydkglel123@gmail.com>
2024-07-12 00:54:14 +0900

    [FIX] Like 엔티티 MySQL 예약어로 인해 테이블명 명시

--- src/main/java/org/websoso/WSSServer/domain/Like.java
+++ src/main/java/org/websoso/WSSServer/domain/Like.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import org.websoso.WSSServer.domain.common.BaseEntity;
 
 @Entity
 @Getter
+@Table(name = "`like`")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Like extends BaseEntity {
 
