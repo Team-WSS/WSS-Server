165e695d4c3fed8260ea5cfbf901bec6e8e420f5
ChaeAg <dydkglel123@gmail.com>
2024-07-11 18:12:36 +0900

    [FIX] Platform, Novel 엔티티 사이 관계테이블 생성 후 엔티티 간 관계 재설정

---
+++ src/main/java/org/websoso/WSSServer/domain/NovelPlatform.java
@@ -0,0 +1,36 @@
+package org.websoso.WSSServer.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NovelPlatform {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long novelPlatformId;
+
+    @Column(nullable = false)
+    private String novelPlatformUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "novel_id", nullable = false)
+    private Novel novel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "platform_id", nullable = false)
+    private Platform platform;
+
+}
--- src/main/java/org/websoso/WSSServer/domain/Platform.java
+++ src/main/java/org/websoso/WSSServer/domain/Platform.java
@@ -4,11 +4,8 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,19 +17,12 @@ public class Platform {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    private Long platformId;
+    private Byte platformId;
 
     @Column(columnDefinition = "varchar(10)", nullable = false)
     private String platformName;
 
     @Column(columnDefinition = "text", nullable = false)
     private String platformImage;
-
-    @Column(nullable = false)
-    private String platformUrl;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "novel_id", nullable = false)
-    private Novel novel;
 
 }
