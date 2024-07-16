953bc62f23f8df40fbaba9299c06d1a38f4ea05a
nakyeong <rinarina0429@naver.com>
2024-07-16 21:42:22 +0900

    [FEAT] 작품평가 수정을 위한 엔드포인트

--- src/main/java/org/websoso/WSSServer/controller/UserNovelController.java
+++ src/main/java/org/websoso/WSSServer/controller/UserNovelController.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 import jakarta.validation.Valid;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +49,18 @@ public class UserNovelController {
 
         return ResponseEntity
                 .status(CREATED)
+                .build();
+    }
+
+    @PutMapping("/{novelId}")
+    public ResponseEntity<Void> updateUserNovel(Principal principal, @PathVariable Long novelId) {
+
+        User user = userService.getUserOrException(Long.valueOf(principal.getName()));
+        Novel novel = novelService.getNovelOrException(novelId);
+        userNovelService.updateUserNovel(user, novel);
+
+        return ResponseEntity
+                .status(NO_CONTENT)
                 .build();
     }
 
