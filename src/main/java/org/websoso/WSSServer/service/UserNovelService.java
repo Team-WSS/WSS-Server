7fa465c7f0ddcaea310995da6512c67b160219ea
nakyeong <rinarina0429@naver.com>
2024-07-16 22:56:56 +0900

    [FEAT] 작품평가 수정을 위한 서비스 로직

--- src/main/java/org/websoso/WSSServer/service/UserNovelService.java
+++ src/main/java/org/websoso/WSSServer/service/UserNovelService.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.service;
 
 import static org.websoso.WSSServer.exception.error.CustomNovelError.NOVEL_NOT_FOUND;
+import static org.websoso.WSSServer.exception.error.CustomUserNovelError.NOT_EVALUATED;
 import static org.websoso.WSSServer.exception.error.CustomUserNovelError.USER_NOVEL_ALREADY_EXISTS;
 import static org.websoso.WSSServer.exception.error.CustomUserNovelError.USER_NOVEL_NOT_FOUND;
 
@@ -21,6 +22,7 @@ import org.websoso.WSSServer.domain.UserNovelAttractivePoint;
 import org.websoso.WSSServer.dto.keyword.KeywordGetResponse;
 import org.websoso.WSSServer.dto.userNovel.UserNovelCreateRequest;
 import org.websoso.WSSServer.dto.userNovel.UserNovelGetResponse;
+import org.websoso.WSSServer.dto.userNovel.UserNovelUpdateRequest;
 import org.websoso.WSSServer.exception.exception.CustomNovelException;
 import org.websoso.WSSServer.exception.exception.CustomUserNovelException;
 import org.websoso.WSSServer.repository.NovelKeywordRepository;
@@ -65,16 +67,60 @@ public class UserNovelService {
                 user,
                 novel));
 
-        for (String stringAttractivePoint : request.attractivePoints()) {
+        createUserNovelAttractivePoints(userNovel, request.attractivePoints());
+        createNovelKeywords(novel, user.getUserId(), request.keywordIds());
+
+    }
+
+    public void updateUserNovel(User user, Novel novel, UserNovelUpdateRequest request) {
+
+        UserNovel userNovel = getUserNovelOrNull(user, novel);
+
+        if (userNovel == null) {
+            throw new CustomUserNovelException(USER_NOVEL_NOT_FOUND,
+                    "user novel with the given user and novel is not found");
+        }
+
+        if (userNovel.getStatus() == null) {
+            throw new CustomUserNovelException(NOT_EVALUATED, "this novel has not been evaluated by the user");
+        }
+
+        LocalDate startDate = request.startDate() == null ? null : convertToLocalDate(request.startDate());
+        LocalDate endDate = request.endDate() == null ? null : convertToLocalDate(request.endDate());
+
+        userNovel.updateUserNovel(request.userNovelRating(), request.status(), startDate, endDate);
+
+        deletePreviousUserNovelAttractivePoints(userNovel);
+        createUserNovelAttractivePoints(userNovel, request.attractivePoints());
+
+        deletePreviousNovelKeywords(novel, user.getUserId());
+        createNovelKeywords(novel, user.getUserId(), request.keywordIds());
+
+    }
+
+    private void createUserNovelAttractivePoints(UserNovel userNovel, List<String> request) {
+        for (String stringAttractivePoint : request) {
             AttractivePoint attractivePoint = attractivePointService.getAttractivePointByString(stringAttractivePoint);
             userNovelAttractivePointRepository.save(UserNovelAttractivePoint.create(userNovel, attractivePoint));
         }
+    }
 
-        for (Integer keywordId : request.keywordIds()) {
+    private void createNovelKeywords(Novel novel, Long userId, List<Integer> request) {
+        for (Integer keywordId : request) {
             Keyword keyword = keywordService.getKeywordOrException(keywordId);
-            novelKeywordRepository.save(NovelKeyword.create(novel, keyword, user.getUserId()));
+            novelKeywordRepository.save(NovelKeyword.create(novel, keyword, userId));
         }
+    }
 
+    private void deletePreviousUserNovelAttractivePoints(UserNovel userNovel) {
+        List<UserNovelAttractivePoint> previousAttractivePoints = userNovelAttractivePointRepository.findAllByUserNovel(
+                userNovel);
+        userNovelAttractivePointRepository.deleteAll(previousAttractivePoints);
+    }
+
+    private void deletePreviousNovelKeywords(Novel novel, Long userId) {
+        List<NovelKeyword> previousNovelKeywords = novelKeywordRepository.findAllByNovelAndUserId(novel, userId);
+        novelKeywordRepository.deleteAll(previousNovelKeywords);
     }
 
     public UserNovel createUserNovelByInterest(User user, Novel novel) {
