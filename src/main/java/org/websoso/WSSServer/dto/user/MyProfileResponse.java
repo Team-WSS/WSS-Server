436c14ad4b6294ed90e934c35c49927c0c44f44e
Kim-TaeUk <xodnr0130@naver.com>
2024-07-17 15:07:56 +0900

    [FEAT] response DTO of 메서드 추가
    정적 팩터리 메서드 패턴

--- src/main/java/org/websoso/WSSServer/dto/user/MyProfileResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/user/MyProfileResponse.java
@@ -1,6 +1,10 @@
 package org.websoso.WSSServer.dto.user;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import org.websoso.WSSServer.domain.Avatar;
+import org.websoso.WSSServer.domain.GenrePreference;
+import org.websoso.WSSServer.domain.User;
 
 public record MyProfileResponse(
         String nickname,
@@ -8,4 +12,14 @@ public record MyProfileResponse(
         String avatarImage,
         List<String> genrePreferences
 ) {
+
+    public static MyProfileResponse of(User user, Avatar avatar, List<GenrePreference> genrePreferences) {
+        return new MyProfileResponse(
+                user.getNickname(),
+                user.getIntro(),
+                avatar.getAvatarImage(),
+                genrePreferences.stream()
+                        .map(gp -> gp.getGenre().getGenreName())
+                        .collect(Collectors.toList()));
+    }
 }
