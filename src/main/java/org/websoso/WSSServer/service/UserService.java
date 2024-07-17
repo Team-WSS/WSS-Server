64bcfdd0d5e94882d116bdea3129acd2e67d3e06
Kim-TaeUk <xodnr0130@naver.com>
2024-07-17 15:06:51 +0900

    [FEAT] 본인 프로필 조회 서비스 로직 추가

--- src/main/java/org/websoso/WSSServer/service/UserService.java
+++ src/main/java/org/websoso/WSSServer/service/UserService.java
@@ -1,21 +1,29 @@
 package org.websoso.WSSServer.service;
 
+import static org.websoso.WSSServer.exception.error.CustomAvatarError.AVATAR_NOT_FOUND;
 import static org.websoso.WSSServer.exception.error.CustomUserError.DUPLICATED_NICKNAME;
 import static org.websoso.WSSServer.exception.error.CustomUserError.INVALID_PROFILE_STATUS;
 import static org.websoso.WSSServer.exception.error.CustomUserError.USER_NOT_FOUND;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.websoso.WSSServer.config.jwt.JwtProvider;
 import org.websoso.WSSServer.config.jwt.UserAuthentication;
+import org.websoso.WSSServer.domain.Avatar;
+import org.websoso.WSSServer.domain.GenrePreference;
+import org.websoso.WSSServer.domain.GenrePreferenceRepository;
 import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.dto.user.EditProfileStatusRequest;
 import org.websoso.WSSServer.dto.user.EmailGetResponse;
 import org.websoso.WSSServer.dto.user.LoginResponse;
+import org.websoso.WSSServer.dto.user.MyProfileResponse;
 import org.websoso.WSSServer.dto.user.NicknameValidation;
 import org.websoso.WSSServer.dto.user.ProfileStatusResponse;
+import org.websoso.WSSServer.exception.exception.CustomAvatarException;
 import org.websoso.WSSServer.exception.exception.CustomUserException;
+import org.websoso.WSSServer.repository.AvatarRepository;
 import org.websoso.WSSServer.repository.UserRepository;
 
 @Service
@@ -25,6 +33,8 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
+    private final AvatarRepository avatarRepository;
+    private final GenrePreferenceRepository genrePreferenceRepository;
 
     @Transactional(readOnly = true)
     public NicknameValidation isNicknameAvailable(String nickname) {
@@ -67,4 +77,14 @@ public class UserService {
                 new CustomUserException(USER_NOT_FOUND, "user with the given id was not found"));
     }
 
+    @Transactional(readOnly = true)
+    public MyProfileResponse getMyProfileInfo(User user) {
+        Byte avatarId = user.getAvatarId();
+        System.out.println("avatarId = " + avatarId);
+        Avatar avatar = avatarRepository.findById(avatarId)
+                .orElseThrow(
+                        () -> new CustomAvatarException(AVATAR_NOT_FOUND, "avatar with the given id was not found"));
+        List<GenrePreference> genrePreferences = genrePreferenceRepository.findByUser(user);
+        return MyProfileResponse.of(user, avatar, genrePreferences);
+    }
 }
