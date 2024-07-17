d60378a4b8acf688fd921ed503938c7e53556061
Kim-TaeUk <xodnr0130@naver.com>
2024-07-17 15:08:43 +0900

    [FEAT] GenrePreference 리포지토리 findByUser 메서드 추가

--- src/main/java/org/websoso/WSSServer/domain/GenrePreferenceRepository.java
+++ src/main/java/org/websoso/WSSServer/domain/GenrePreferenceRepository.java
@@ -1,9 +1,11 @@
 package org.websoso.WSSServer.domain;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GenrePreferenceRepository extends JpaRepository<GenrePreference, Long> {
 
+    List<GenrePreference> findByUser(User user);
 }
