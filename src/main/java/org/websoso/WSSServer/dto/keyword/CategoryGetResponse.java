7a34bfeeeeada212d6859082667d638802794cb0
nakyeong <rinarina0429@naver.com>
2024-07-17 14:47:46 +0900

    [FEAT] CategoryGetResponse에 categoryImage 추가

--- src/main/java/org/websoso/WSSServer/dto/keyword/CategoryGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/keyword/CategoryGetResponse.java
@@ -1,15 +1,17 @@
 package org.websoso.WSSServer.dto.keyword;
 
 import java.util.List;
-import org.websoso.WSSServer.domain.common.KeywordCategoryName;
+import org.websoso.WSSServer.domain.KeywordCategory;
 
 public record CategoryGetResponse(
         String categoryName,
+        String categoryImage,
         List<KeywordGetResponse> keywords
 ) {
-    public static CategoryGetResponse of(KeywordCategoryName keywordCategoryName, List<KeywordGetResponse> keywords) {
+    public static CategoryGetResponse of(KeywordCategory keywordCategory, List<KeywordGetResponse> keywords) {
         return new CategoryGetResponse(
-                keywordCategoryName.getLabel(),
+                keywordCategory.getKeywordCategoryName(),
+                keywordCategory.getKeywordCategoryImage(),
                 keywords
         );
     }
