d0a0bb9a8e43db5fb0e4b1567afab086490578db
nakyeong <rinarina0429@naver.com>
2024-07-21 01:13:54 +0900

    [REFACTOR] NOT_INTERESTED HttpStatus 수정

--- src/main/java/org/websoso/WSSServer/exception/error/CustomUserNovelError.java
+++ src/main/java/org/websoso/WSSServer/exception/error/CustomUserNovelError.java
@@ -1,5 +1,6 @@
 package org.websoso.WSSServer.exception.error;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
@@ -15,7 +16,7 @@ public enum CustomUserNovelError implements ICustomError {
     USER_NOVEL_NOT_FOUND("USER_NOVEL-001", "해당 작품이 유저의 서재에 등록되어 있지 않습니다.", NOT_FOUND),
     USER_NOVEL_ALREADY_EXISTS("USER_NOVEL-002", "이미 서재에 등록된 작품입니다.", CONFLICT),
     ALREADY_INTERESTED("USER_NOVEL-003", "이미 해당 작품이 관심 작품으로 등록되어 있습니다.", CONFLICT),
-    NOT_INTERESTED("USER_NOVEL-004", "관심으로 등록되어 있지 않은 작품입니다.", CONFLICT);
+    NOT_INTERESTED("USER_NOVEL-004", "관심으로 등록되어 있지 않은 작품입니다.", BAD_REQUEST);
 
     private final String code;
     private final String description;
