0bc6c91a1a84ee30871b25e40c9401fadf93b76f
Kim-TaeUk <xodnr0130@naver.com>
2024-07-22 22:35:27 +0900

    [FIX] request에서 gender의 type 변경
    from Gender(enum) to String

--- src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
@@ -1,11 +1,10 @@
 package org.websoso.WSSServer.dto.user;
 
 import java.time.Year;
-import org.websoso.WSSServer.domain.common.Gender;
 
 public record RegisterUserInfoRequest(
         String nickname,
-        Gender gender,
+        String gender,
         Year birth
 ) {
 }
