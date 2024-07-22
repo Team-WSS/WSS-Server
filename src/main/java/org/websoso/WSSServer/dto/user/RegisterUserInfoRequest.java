9a9e1a8ec9232747380ecb54affa9e3c2e01e846
Kim-TaeUk <xodnr0130@naver.com>
2024-07-22 22:36:50 +0900

    [FEAT] RegisterUserInfoRequest DTO에서 Gender validation 추가
    NotNull, GenderConstraint

--- src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
@@ -1,9 +1,13 @@
 package org.websoso.WSSServer.dto.user;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.Year;
+import org.websoso.WSSServer.validation.GenderConstraint;
 
 public record RegisterUserInfoRequest(
         String nickname,
+        @NotNull
+        @GenderConstraint
         String gender,
         Year birth
 ) {
