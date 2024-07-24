4c43d4252fb51ddca0bc9afc08768ca3076f0e77
Kim-TaeUk <xodnr0130@naver.com>
2024-07-25 05:53:41 +0900

    [FIX] request에서 birth의 type 변경
    from Year to Integer

--- src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
@@ -1,16 +1,17 @@
 package org.websoso.WSSServer.dto.user;
 
 import jakarta.validation.constraints.NotNull;
-import java.time.Year;
 import org.websoso.WSSServer.validation.GenderConstraint;
 import org.websoso.WSSServer.validation.NicknameConstraint;
 
 public record RegisterUserInfoRequest(
         @NicknameConstraint
         String nickname,
+
         @NotNull
         @GenderConstraint
         String gender,
-        Year birth
+
+        Integer birth
 ) {
 }
