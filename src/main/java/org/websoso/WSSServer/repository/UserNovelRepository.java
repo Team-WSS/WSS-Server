4519e2b09d4835d7cda6dc52bea4d64c7fc4ca30
Kim-TaeUk <xodnr0130@naver.com>
2024-07-25 08:59:02 +0900

    [FEAT] UserNovelRepository에 스케쥴링에 필요한 메서드 정의 by JPQL
    ReadStatus가 WATCHING이거나 WATCHED이고, isInterest가 true인 novelId를 내림차순으로 30개(Pageable로 30개 지정)

--- src/main/java/org/websoso/WSSServer/repository/UserNovelRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/UserNovelRepository.java
@@ -1,6 +1,8 @@
 package org.websoso.WSSServer.repository;
 
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -22,5 +24,13 @@ public interface UserNovelRepository extends JpaRepository<UserNovel, Long> {
     Float sumUserNovelRatingByNovel(Novel novel);
 
     Integer countByNovelAndUserNovelRatingNot(Novel novel, float ratingToExclude);
+
+    //TODO QueryDSL로 수정하기
+    @Query(value = "SELECT un.novel.novelId "
+            + "FROM UserNovel un "
+            + "WHERE (un.status = 'WATCHING' OR un.status = 'WATCHED' OR un.isInterest = true) "
+            + "GROUP BY un.novel.novelId "
+            + "ORDER BY COUNT(un) DESC")
+    List<Long> findTodayPopularNovelsId(Pageable pageable);
 
 }
