a9007389aacc15d3257fd303cfeea5ec39a1846d
Kim-TaeUk <xodnr0130@naver.com>
2024-07-25 05:55:14 +0900

    [FEAT] request dto validation 추가
    not null, 1900~2024년 사이

--- src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.dto.user;
 
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Range;
 import org.websoso.WSSServer.validation.GenderConstraint;
 import org.websoso.WSSServer.validation.NicknameConstraint;
 
@@ -12,6 +13,9 @@ public record RegisterUserInfoRequest(
         @GenderConstraint
         String gender,
 
+        @NotNull(message = "출생연도는 null일 수 없습니다.")
+        @Range(min = 1900, max = 2024, message = "출생연도는 1900 ~ 2024 사이여야 합니다.")
         Integer birth
+        //TODO 날짜 상수화, 동적으로 가져와야 함
 ) {
 }
