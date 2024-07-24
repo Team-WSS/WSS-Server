f593b8c3eee177737fb31fcedc3eaf7d58f2eb8c
Kim-TaeUk <xodnr0130@naver.com>
2024-07-25 06:21:15 +0900

    [FEAT] request DTO에 genrePreferences(선호 장르) 추가

--- src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.dto.user;
 
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import org.hibernate.validator.constraints.Range;
 import org.websoso.WSSServer.validation.GenderConstraint;
 import org.websoso.WSSServer.validation.NicknameConstraint;
@@ -15,7 +16,10 @@ public record RegisterUserInfoRequest(
 
         @NotNull(message = "출생연도는 null일 수 없습니다.")
         @Range(min = 1900, max = 2024, message = "출생연도는 1900 ~ 2024 사이여야 합니다.")
-        Integer birth
+        Integer birth,
         //TODO 날짜 상수화, 동적으로 가져와야 함
+
+        @NotNull
+        List<String> genrePreferences
 ) {
 }
