e42e73e043a7afba41e72f558e50f551151d47c9
Kim-TaeUk <xodnr0130@naver.com>
2024-07-25 09:07:02 +0900

    [FIX] RecentUserNovelRepository에 누락된 @Repository 어노테이션 추가

--- src/main/java/org/websoso/WSSServer/repository/RecentUserNovelRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/RecentUserNovelRepository.java
@@ -1,8 +1,10 @@
 package org.websoso.WSSServer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.RecentUserNovel;
 
+@Repository
 public interface RecentUserNovelRepository extends JpaRepository<RecentUserNovel, Long> {
 
 }
