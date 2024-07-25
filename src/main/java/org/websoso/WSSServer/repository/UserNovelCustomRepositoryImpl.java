f53e1f7d25c4fd501d7adfe9520c5f154facc173
Kim-TaeUk <xodnr0130@naver.com>
2024-07-25 21:22:18 +0900

    [FIX] 누락된 인기작 정책 반영
    '최근 7일 동안' 조건이 빠져서 추가

--- src/main/java/org/websoso/WSSServer/repository/UserNovelCustomRepositoryImpl.java
+++ src/main/java/org/websoso/WSSServer/repository/UserNovelCustomRepositoryImpl.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -17,13 +18,15 @@ public class UserNovelCustomRepositoryImpl implements UserNovelCustomRepository 
     @Override
     public List<Long> findTodayPopularNovelsId(Pageable pageable) {
         QUserNovel userNovel = QUserNovel.userNovel;
+        LocalDate sevenDaysAgo = LocalDate.now().minusDays(7);
 
         return jpaQueryFactory
                 .select(userNovel.novel.novelId)
                 .from(userNovel)
                 .where(userNovel.status.eq(ReadStatus.WATCHING)
                         .or(userNovel.status.eq(ReadStatus.WATCHED))
-                        .or(userNovel.isInterest.isTrue()))
+                        .or(userNovel.isInterest.isTrue())
+                        .and(userNovel.createdDate.after(sevenDaysAgo.atStartOfDay())))
                 .groupBy(userNovel.novel.novelId)
                 .orderBy(userNovel.count().desc())
                 .offset(pageable.getOffset())
