a9db7714fb232207c694aee04c0b0212fa65546b
Kim-TaeUk <xodnr0130@naver.com>
2024-07-26 16:22:53 +0900

    [FEAT] 오늘의 인기작 조회 DTO에 피드 관련 추가

--- src/main/java/org/websoso/WSSServer/dto/popularNovel/PopularNovelGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/popularNovel/PopularNovelGetResponse.java
@@ -1,18 +1,26 @@
 package org.websoso.WSSServer.dto.popularNovel;
 
+import org.websoso.WSSServer.domain.Avatar;
+import org.websoso.WSSServer.domain.Feed;
 import org.websoso.WSSServer.domain.Novel;
 
 public record PopularNovelGetResponse(
         Long novelId,
         String title,
-        String novelImage
+        String novelImage,
+        String avatarImage,
+        String nickname,
+        String feedContent
 ) {
 
-    public static PopularNovelGetResponse of(Novel novel) {
+    public static PopularNovelGetResponse of(Novel novel, Avatar avatar, Feed feed) {
         return new PopularNovelGetResponse(
                 novel.getNovelId(),
                 novel.getTitle(),
-                novel.getNovelImage()
+                novel.getNovelImage(),
+                avatar.getAvatarImage(),
+                feed.getUser().getNickname(),
+                feed.getFeedContent()
         );
     }
 }
