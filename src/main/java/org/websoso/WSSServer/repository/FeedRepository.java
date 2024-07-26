9dcbfb778a3439f3ff54c13f7e9e33e1eed5dcfd
Kim-TaeUk <xodnr0130@naver.com>
2024-07-26 16:23:20 +0900

    [FEAT] novel에 해당하는 인기 피드 조회

--- src/main/java/org/websoso/WSSServer/repository/FeedRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/FeedRepository.java
@@ -1,6 +1,9 @@
 package org.websoso.WSSServer.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Feed;
 
@@ -8,4 +11,7 @@ import org.websoso.WSSServer.domain.Feed;
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
     Integer countByNovelId(Long novelId);
+
+    @Query("SELECT f FROM Feed f WHERE f.novelId IN :novelIds ORDER BY SIZE(f.likes) DESC")
+    List<Feed> findPopularFeedsByNovelIds(@Param("novelIds") List<Long> novelIds);
 }
