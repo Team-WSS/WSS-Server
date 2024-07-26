5db2a3c3a18287afb48c2c9ec236d0738a2f0e13
Kim-TaeUk <xodnr0130@naver.com>
2024-07-26 18:53:29 +0900

    [REMOVE] 잘못 구현된 novel에 해당하는 인기 피드 조회 로직 삭제

--- src/main/java/org/websoso/WSSServer/repository/FeedRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/FeedRepository.java
@@ -1,17 +1,12 @@
 package org.websoso.WSSServer.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Feed;
 
 @Repository
-public interface FeedRepository extends JpaRepository<Feed, Long> {
+public interface FeedRepository extends JpaRepository<Feed, Long>, FeedCustomRepository {
 
     Integer countByNovelId(Long novelId);
 
-    @Query("SELECT f FROM Feed f WHERE f.novelId IN :novelIds ORDER BY SIZE(f.likes) DESC")
-    List<Feed> findPopularFeedsByNovelIds(@Param("novelIds") List<Long> novelIds);
 }
