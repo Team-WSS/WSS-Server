5aa9bcfc69c29b1939b25a092bd568f4d8241f4f
nakyeong <rinarina0429@naver.com>
2024-07-27 02:40:24 +0900

    [FEAT] GenreRepository 추가

---
+++ src/main/java/org/websoso/WSSServer/repository/GenreRepository.java
@@ -0,0 +1,13 @@
+package org.websoso.WSSServer.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.websoso.WSSServer.domain.Genre;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Byte> {
+
+    Optional<Genre> findByGenreName(String name);
+
+}
