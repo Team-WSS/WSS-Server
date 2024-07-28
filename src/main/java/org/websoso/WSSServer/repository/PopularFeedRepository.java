2755e56cdabbdce3102e435e027beb7edccdae0b
Kim-TaeUk <xodnr0130@naver.com>
2024-07-29 01:57:23 +0900

    [FEAT] PopularFeedRepository에 실시간 인기글 조회 쿼리메서드 추가
    PopularFeedRepository에서 최근 인기글 상위 9개 반환

--- src/main/java/org/websoso/WSSServer/repository/PopularFeedRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/PopularFeedRepository.java
@@ -1,5 +1,6 @@
 package org.websoso.WSSServer.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Feed;
@@ -9,4 +10,6 @@ import org.websoso.WSSServer.domain.PopularFeed;
 public interface PopularFeedRepository extends JpaRepository<PopularFeed, Long> {
 
     Boolean existsByFeed(Feed feed);
+
+    List<PopularFeed> findTop9ByOrderByPopularFeedIdDesc();
 }
