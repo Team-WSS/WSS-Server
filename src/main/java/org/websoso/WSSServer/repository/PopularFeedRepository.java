0cf8352d4bcd4139ffde1176e995ddf22bfb9df4
Kim-TaeUk <xodnr0130@naver.com>
2024-07-29 00:27:22 +0900

    [FIX] PopularFeedRepository에 누락된 @Repository 어노테이션 추가

--- src/main/java/org/websoso/WSSServer/repository/PopularFeedRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/PopularFeedRepository.java
@@ -1,9 +1,11 @@
 package org.websoso.WSSServer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.Feed;
 import org.websoso.WSSServer.domain.PopularFeed;
 
+@Repository
 public interface PopularFeedRepository extends JpaRepository<PopularFeed, Long> {
 
     Boolean existsByFeed(Feed feed);
