ffb7670d25520bd066405473986f2e680c4a74a6
Kim-TaeUk <xodnr0130@naver.com>
2024-07-30 04:23:15 +0900

    [FEAT] PopularFeedGetResponse에 정적 팩터리 메서드 of 추가

--- src/main/java/org/websoso/WSSServer/dto/popularFeed/PopularFeedGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/popularFeed/PopularFeedGetResponse.java
@@ -1,5 +1,7 @@
 package org.websoso.WSSServer.dto.popularFeed;
 
+import org.websoso.WSSServer.domain.PopularFeed;
+
 public record PopularFeedGetResponse(
         Long feedId,
         String feedContent,
@@ -7,4 +9,14 @@ public record PopularFeedGetResponse(
         Integer commentCount,
         Boolean isSpoiler
 ) {
+
+    public static PopularFeedGetResponse of(PopularFeed popularFeed) {
+        return new PopularFeedGetResponse(
+                popularFeed.getFeed().getFeedId(),
+                popularFeed.getFeed().getFeedContent(),
+                popularFeed.getFeed().getLikes().size(),
+                popularFeed.getFeed().getComments().size(),
+                popularFeed.getFeed().getIsSpoiler()
+        );
+    }
 }
