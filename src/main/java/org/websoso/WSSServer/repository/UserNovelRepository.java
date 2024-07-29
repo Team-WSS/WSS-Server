8619509d7f878eff5d24970eb33302f55acd37e6
Kim-TaeUk <xodnr0130@naver.com>
2024-07-30 07:50:53 +0900

    [FEAT] 유저의 등록 작품 통계 조회 메서드 추가 By query dsl
    - case문을 통해 관심, 보는 중, 봤어요, 하차 작품 수 계산
    - Projections.constructor를 통해 response mapping

--- src/main/java/org/websoso/WSSServer/repository/UserNovelRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/UserNovelRepository.java
@@ -10,7 +10,7 @@ import org.websoso.WSSServer.domain.UserNovel;
 import org.websoso.WSSServer.domain.common.ReadStatus;
 
 @Repository
-public interface UserNovelRepository extends JpaRepository<UserNovel, Long> {
+public interface UserNovelRepository extends JpaRepository<UserNovel, Long>, UserNovelCustomRepository {
 
     Optional<UserNovel> findByNovelAndUser(Novel novel, User user);
 
