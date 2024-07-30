74eb33a00ff1d7deb26d50fb489264b6dbdbbedb
nakyeong <rinarina0429@naver.com>
2024-07-30 21:09:19 +0900

    [REFACTOR] dto 필드 기준 개행 추가

--- src/main/java/org/websoso/WSSServer/dto/userNovel/UserNovelCreateRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/userNovel/UserNovelCreateRequest.java
@@ -13,18 +13,24 @@ import org.websoso.WSSServer.validation.UserNovelRatingConstraint;
 public record UserNovelCreateRequest(
         @NotNull(message = "작품 id는 null일 수 없습니다.")
         Long novelId,
+
         @UserNovelRatingConstraint
         @ColumnDefault("0.0")
         Float userNovelRating,
+
         @NotNull(message = "읽기 상태는 null일 수 없습니다.")
         ReadStatus status,
+
         @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "시작 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         String startDate,
+
         @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "종료 날짜는 yyyy-MM-dd 형식이어야 합니다.")
         String endDate,
+
         @JsonSetter(nulls = Nulls.AS_EMPTY)
         @Size(max = 3, message = "매력 포인트는 최대 3개까지 가능합니다.")
         List<String> attractivePoints,
+
         @JsonSetter(nulls = Nulls.AS_EMPTY)
         @Size(max = 10, message = "키워드는 최대 10개까지 가능합니다.")
         List<Integer> keywordIds
