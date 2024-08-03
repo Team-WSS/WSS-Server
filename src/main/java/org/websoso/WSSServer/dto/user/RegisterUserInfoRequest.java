249cab331e28d0ad3ea81b67ca02961a6a152fd4
Kim-TaeUk <xodnr0130@naver.com>
2024-08-03 12:52:37 +0900

    [FIX] RegisterUserInfoRequest.birth를 @BirthConstraint로 검증

--- src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/RegisterUserInfoRequest.java
@@ -2,7 +2,7 @@ package org.websoso.WSSServer.dto.user;
 
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import org.hibernate.validator.constraints.Range;
+import org.websoso.WSSServer.validation.BirthConstraint;
 import org.websoso.WSSServer.validation.GenderConstraint;
 import org.websoso.WSSServer.validation.NicknameConstraint;
 
@@ -15,9 +15,8 @@ public record RegisterUserInfoRequest(
         String gender,
 
         @NotNull(message = "출생연도는 null일 수 없습니다.")
-        @Range(min = 1900, max = 2024, message = "출생연도는 1900 ~ 2024 사이여야 합니다.")
+        @BirthConstraint
         Integer birth,
-        //TODO 날짜 상수화, 동적으로 가져와야 함
 
         @NotNull(message = "선호 장르는 null일 수 없습니다.")
         List<String> genrePreferences
