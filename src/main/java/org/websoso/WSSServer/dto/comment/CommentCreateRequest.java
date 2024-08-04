e3c8da648055fc81a712dbcb023ec1da79c6c5bc
ChaeAg <dydkglel123@gmail.com>
2024-08-05 01:27:57 +0900

    [FIX] DTO 유효성 검사 메세지 직관적이게 수정

--- src/main/java/org/websoso/WSSServer/dto/comment/CommentCreateRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/comment/CommentCreateRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CommentCreateRequest(
-        @NotBlank(message = "댓글 내용은 비어 있거나, 공백일 수 없습니다.")
+        @NotBlank(message = "댓글 내용은 null 이거나, 공백일 수 없습니다.")
         @Size(max = 100, message = "댓글 내용은 100자를 초과할 수 없습니다.")
         String commentContent
 ) {
