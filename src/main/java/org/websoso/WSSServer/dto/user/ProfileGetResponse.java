ca149ed5c9e2c2b07dd134314e8c3e1d8944eadc
Kim-TaeUk <xodnr0130@naver.com>
2024-08-06 07:06:11 +0900

    [FEAT] 프로필 조회를 위한 DTO 추가
    내 프로필인지 나타내는 flag 변수 isMyProfile

---
+++ src/main/java/org/websoso/WSSServer/dto/user/ProfileGetResponse.java
@@ -0,0 +1,28 @@
+package org.websoso.WSSServer.dto.user;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.websoso.WSSServer.domain.Avatar;
+import org.websoso.WSSServer.domain.GenrePreference;
+import org.websoso.WSSServer.domain.User;
+
+public record ProfileGetResponse(
+        String nickname,
+        String intro,
+        String avatarImage,
+        Boolean isProfilePublic,
+        List<String> genrePreferences
+) {
+
+    public static ProfileGetResponse of(User user, Avatar avatar,
+                                        List<GenrePreference> genrePreferences, boolean isMyProfile) {
+        return new ProfileGetResponse(
+                user.getNickname(),
+                user.getIntro(),
+                avatar.getAvatarImage(),
+                isMyProfile ? true : user.getIsProfilePublic(),
+                genrePreferences.stream()
+                        .map(gp -> gp.getGenre().getGenreName())
+                        .collect(Collectors.toList()));
+    }
+}
