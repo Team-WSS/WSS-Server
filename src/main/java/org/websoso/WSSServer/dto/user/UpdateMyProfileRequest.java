daf41b59de1e79a9bfe8b4a26b0b9f3c1d3ceed4
Kim-TaeUk <xodnr0130@naver.com>
2024-08-06 00:54:16 +0900

    [FIX] @NicknameConstraint 대신 @NullAllowedNicknameConstraint로 변경

--- src/main/java/org/websoso/WSSServer/dto/user/UpdateMyProfileRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/UpdateMyProfileRequest.java
@@ -1,12 +1,12 @@
 package org.websoso.WSSServer.dto.user;
 
 import jakarta.validation.constraints.Size;
-import org.websoso.WSSServer.validation.NicknameConstraint;
+import org.websoso.WSSServer.validation.NullAllowedNicknameConstraint;
 
 public record UpdateMyProfileRequest(
         Byte avatarId,
 
-        @NicknameConstraint
+        @NullAllowedNicknameConstraint
         String nickname,
 
         @Size(max = 60, message = "소개글은 60자를 초과할 수 없습니다.")
