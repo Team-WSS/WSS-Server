6163fd0c94f4a1d291fbdcbdd4fd1430dbd1c1d5
Kim-TaeUk <xodnr0130@naver.com>
2024-08-08 01:09:27 +0900

    [FEAT] 관심글 조회 DTO 추가

---
+++ src/main/java/org/websoso/WSSServer/dto/feed/InterestFeedGetResponse.java
@@ -0,0 +1,52 @@
+package org.websoso.WSSServer.dto.feed;
+
+import org.websoso.WSSServer.domain.Avatar;
+import org.websoso.WSSServer.domain.Feed;
+import org.websoso.WSSServer.domain.Novel;
+import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.domain.UserNovel;
+
+public record InterestFeedGetResponse(
+        Long novelId,
+        String novelTitle,
+        String novelImage,
+        Float novelRating,
+        Long novelRatingCount,
+        String nickname,
+        String avatarImage,
+        String feedContent
+) {
+
+    public static InterestFeedGetResponse of(Novel novel, User user, Feed feed, Avatar avatar) {
+        Long novelRatingCount = getNovelRatingCount(novel);
+        Float novelRating = novelRatingCount > 0
+                ? getNovelRatingSum(novel) / novelRatingCount
+                : 0.0f;
+
+        return new InterestFeedGetResponse(
+                novel.getNovelId(),
+                novel.getTitle(),
+                novel.getNovelImage(),
+                novelRating,
+                novelRatingCount,
+                user.getNickname(),
+                avatar.getAvatarImage(),
+                feed.getFeedContent()
+        );
+    }
+
+    private static Float getNovelRatingSum(Novel novel) {
+        return (float) novel.getUserNovels()
+                .stream()
+                .filter(userNovel -> userNovel.getUserNovelRating() != 0.0f)
+                .mapToDouble(UserNovel::getUserNovelRating)
+                .sum();
+    }
+
+    private static Long getNovelRatingCount(Novel novel) {
+        return novel.getUserNovels()
+                .stream()
+                .filter(userNovel -> userNovel.getUserNovelRating() != 0.0f)
+                .count();
+    }
+}
