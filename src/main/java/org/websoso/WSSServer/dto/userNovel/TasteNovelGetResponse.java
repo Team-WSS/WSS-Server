f6311fbf683b41413ff1924be58a42a0089ab897
Kim-TaeUk <xodnr0130@naver.com>
2024-08-08 08:22:07 +0900

    [FEAT] TasteNovelGetResponse DTO에 getInterestCount 메서드 추가
    DTO로 가져온 novel과 연관관계에 있는 userNovel 컬렉션을 통해 interestCount 계산하여 return

--- src/main/java/org/websoso/WSSServer/dto/userNovel/TasteNovelGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/userNovel/TasteNovelGetResponse.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.dto.userNovel;
 
 import org.websoso.WSSServer.domain.Novel;
+import org.websoso.WSSServer.domain.UserNovel;
 
 public record TasteNovelGetResponse(
         Long novelId,
@@ -19,5 +20,12 @@ public record TasteNovelGetResponse(
                 tasteNovel.getAuthor(),
                 tasteNovel.getNovelImage()
         );
+    }
+
+    private static Long getInterestCount(Novel tasteNovel) {
+        return tasteNovel.getUserNovels()
+                .stream()
+                .map(UserNovel::getIsInterest)
+                .count();
     }
 }
