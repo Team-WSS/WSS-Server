07063c74c0b568e5f8ba1b5c99f2f39e0d140769
Kim-TaeUk <xodnr0130@naver.com>
2024-08-08 05:19:18 +0900

    [FEAT] UserFeedGetResponse DTO에 통계성 필드 계산 메서드 추가
    getNovelRatingCount()
    getNovelRatingSum()
    getNovelRating()

--- src/main/java/org/websoso/WSSServer/dto/feed/UserFeedGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/feed/UserFeedGetResponse.java
@@ -3,6 +3,7 @@ package org.websoso.WSSServer.dto.feed;
 import java.time.LocalDate;
 import org.websoso.WSSServer.domain.Feed;
 import org.websoso.WSSServer.domain.Novel;
+import org.websoso.WSSServer.domain.UserNovel;
 
 public record UserFeedGetResponse(
         Long feedId,
@@ -33,5 +34,26 @@ public record UserFeedGetResponse(
                 novel.getNovelId(),
                 novel.getTitle()
         );
+    }
+
+    private static Long getNovelRatingCount(Novel novel) {
+        return novel.getUserNovels()
+                .stream()
+                .filter(userNovel -> userNovel.getUserNovelRating() != 0.0f)
+                .count();
+    }
+
+    private static Float getNovelRatingSum(Novel novel) {
+        return (float) novel.getUserNovels()
+                .stream()
+                .filter(userNovel -> userNovel.getUserNovelRating() != 0.0f)
+                .mapToDouble(UserNovel::getUserNovelRating)
+                .sum();
+    }
+
+    private static float getNovelRating(Novel novel, Long novelRatingCount) {
+        return novelRatingCount > 0
+                ? getNovelRatingSum(novel) / novelRatingCount
+                : 0.0f;
     }
 }
