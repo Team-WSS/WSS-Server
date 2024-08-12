63a6f10ac103abea7ed8d7c0f06ddb14104b5f9b
Kim-TaeUk <xodnr0130@naver.com>
2024-08-13 04:57:20 +0900

    [FEAT] UserGenrePreferenceGetResponse DTO에 필드 매핑

--- src/main/java/org/websoso/WSSServer/dto/userNovel/UserGenrePreferenceGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/userNovel/UserGenrePreferenceGetResponse.java
@@ -1,8 +1,18 @@
 package org.websoso.WSSServer.dto.userNovel;
 
+import org.websoso.WSSServer.domain.Genre;
+
 public record UserGenrePreferenceGetResponse(
         String genreName,
         String genreImage,
         Long genreCount
 ) {
+
+    public static UserGenrePreferenceGetResponse of(Genre genre, Long genreCount) {
+        return new UserGenrePreferenceGetResponse(
+                genre.getGenreName(),
+                genre.getGenreImage(),
+                genreCount
+        );
+    }
 }
