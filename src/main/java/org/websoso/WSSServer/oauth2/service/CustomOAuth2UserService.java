b9d04bbd119fcedf88b21dd62792ac3140e19e7d
Kim-TaeUk <xodnr0130@naver.com>
2024-08-19 04:52:52 +0900

    [FEAT] OAuth2 provider 식별을 위한 registrationId 추가

--- src/main/java/org/websoso/WSSServer/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/org/websoso/WSSServer/oauth2/service/CustomOAuth2UserService.java
@@ -12,6 +12,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
     }
 }
