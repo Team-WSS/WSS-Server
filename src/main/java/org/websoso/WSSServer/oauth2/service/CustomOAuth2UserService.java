a8034c61b7196bdd88685fbdcfb1faedc01c1eec
Kim-TaeUk <xodnr0130@naver.com>
2024-08-19 06:27:50 +0900

    [FEAT] UserRepository에 조회하여 새로운 유저인 경우 save

--- src/main/java/org/websoso/WSSServer/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/org/websoso/WSSServer/oauth2/service/CustomOAuth2UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.oauth2.dto.CustomOauth2User;
 import org.websoso.WSSServer.oauth2.dto.KakaoOauth2Response;
 import org.websoso.WSSServer.oauth2.dto.OAuth2Response;
@@ -29,8 +30,13 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         }
 
         String socialId = oAuth2Response.getProvider() + "_" + oAuth2Response.getProviderId();
-        OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.of(oAuth2Response.getName(), socialId);
+        User existedUserOrNull = userRepository.findBySocialId(socialId);
+        if (existedUserOrNull == null) {
+            userRepository.save(User.createBySocial(socialId, oAuth2Response.getName()));
+            OAuth2UserDTO oAuth2UserDTO = OAuth2UserDTO.of(oAuth2Response.getName(), socialId);
+            return new CustomOauth2User(oAuth2UserDTO);
+        } else {
 
-        return new CustomOauth2User(oAuth2UserDTO);
+        }
     }
 }
