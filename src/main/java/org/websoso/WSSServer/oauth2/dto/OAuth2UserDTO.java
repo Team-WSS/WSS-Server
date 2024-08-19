da2eba08834c167688d15a25b6bd46c7ff33e1f5
Kim-TaeUk <xodnr0130@naver.com>
2024-08-20 04:53:34 +0900

    [FIX] OAuth2UserDTO의 필드 추가
    from: 사용자 이름, socialId
    to: 사용자 이름, socialId, email

--- src/main/java/org/websoso/WSSServer/oauth2/dto/OAuth2UserDTO.java
+++ src/main/java/org/websoso/WSSServer/oauth2/dto/OAuth2UserDTO.java
@@ -2,12 +2,14 @@ package org.websoso.WSSServer.oauth2.dto;
 
 public record OAuth2UserDTO(
         String name,
+        String email,
         String socialId
 ) {
 
-    public static OAuth2UserDTO of(String name, String socialId) {
+    public static OAuth2UserDTO of(String name, String email, String socialId) {
         return new OAuth2UserDTO(
                 name,
+                email,
                 socialId
         );
     }
