db1a13a8cf2a635d1d41c3887453316e79a0e1cf
nakyeong <rinarina0429@naver.com>
2024-09-08 19:12:06 +0900

    [FEAT] GenreName에 koreanLable 추가

--- src/main/java/org/websoso/WSSServer/domain/common/GenreName.java
+++ src/main/java/org/websoso/WSSServer/domain/common/GenreName.java
@@ -7,16 +7,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum GenreName {
 
-    RF("romanceFantasy"),
-    RO("romance"),
-    FA("fantasy"),
-    MF("modernFantasy"),
-    DR("drama"),
-    LN("lightNovel"),
-    WU("wuxia"),
-    MY("mystery"),
-    BL("BL");
+    RF("romanceFantasy", "로판"),
+    RO("romance", "로맨스"),
+    FA("fantasy", "판타지"),
+    MF("modernFantasy", "현판"),
+    DR("drama", "드라마"),
+    LN("lightNovel", "라노벨"),
+    WU("wuxia", "무협"),
+    MY("mystery", "미스터리"),
+    BL("BL", "BL");
 
     private final String label;
+    private final String koreanLabel;
 
 }
