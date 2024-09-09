1faf2d397a712209a16ae7ffc67bc83c431bec8f
Kim-TaeUk <xodnr0130@naver.com>
2024-09-09 16:04:27 +0900

    [REMOVE] JwtProvider에서 토큰 검증 및 정보 추출 로직 삭제
    JwtProvider는 토큰 생성 책임만 가짐

--- src/main/java/org/websoso/WSSServer/config/jwt/JwtProvider.java
+++ src/main/java/org/websoso/WSSServer/config/jwt/JwtProvider.java
@@ -1,12 +1,9 @@
 package org.websoso.WSSServer.config.jwt;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
@@ -68,38 +65,4 @@ public class JwtProvider {
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(JWT_SECRET_KEY.getBytes());
     }
-
-    public Long getUserIdFromJwt(String token) {
-        Claims claims = getClaim(token);
-        return Long.valueOf(claims.get(CLAIM_USER_ID).toString());
-    }
-
-    public JwtValidationType validateJWT(String token) {
-        try {
-            final Claims claims = getClaim(token);
-            return JwtValidationType.VALID_TOKEN;
-        } catch (MalformedJwtException ex) {
-            return JwtValidationType.INVALID_TOKEN;
-        } catch (ExpiredJwtException ex) {
-            return JwtValidationType.EXPIRED_TOKEN;
-        } catch (UnsupportedJwtException ex) {
-            return JwtValidationType.UNSUPPORTED_TOKEN;
-        } catch (IllegalArgumentException ex) {
-            return JwtValidationType.EMPTY_TOKEN;
-        }
-    }
-
-    private Claims getClaim(final String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    public String getTokenTypeFromJwt(String token) {
-        Claims claims = getClaim(token);
-        return claims.getSubject();
-    }
-
 }
