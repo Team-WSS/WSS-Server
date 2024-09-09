b0167fe09b0b686f3055bc293b4df839a2d39499
Kim-TaeUk <xodnr0130@naver.com>
2024-09-09 14:16:16 +0900

    [FEAT] CustomAuthError 추가
    만료된 리프레시 토큰과 유효하지 않은 토큰 enum 추가

---
+++ src/main/java/org/websoso/WSSServer/exception/error/CustomAuthError.java
@@ -0,0 +1,21 @@
+package org.websoso.WSSServer.exception.error;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.websoso.WSSServer.exception.common.ICustomError;
+
+@Getter
+@AllArgsConstructor
+public enum CustomAuthError implements ICustomError {
+
+    EXPIRED_REFRESH_TOKEN("AUTH-001", "만료된 리프레시 토큰입니다.", UNAUTHORIZED),
+    INVALID_TOKEN("AUTH-002", "유효하지 않은 토큰입니다.", UNAUTHORIZED);
+
+    private final String code;
+    private final String description;
+    private final HttpStatus statusCode;
+}
+
