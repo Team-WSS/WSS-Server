1eb013b548f31936ef5e56325374c783381523dc
ChaeAg <dydkglel123@gmail.com>
2024-09-09 11:50:36 +0900

    [REMOVE] 사용하지 않는 코드 제거

--- src/main/java/org/websoso/WSSServer/repository/UserRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package org.websoso.WSSServer.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.User;
@@ -12,5 +11,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findBySocialId(String socialId);
 
-    Optional<User> findBySocialIdAndEmail(String socialId, String email);
 }
