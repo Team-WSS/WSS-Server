241e57e1e9dfbc5b914d47130daafb8e4f4cbdae
Kim-TaeUk <xodnr0130@naver.com>
2024-09-11 01:48:27 +0900

    [FEAT] 만료된 refresh일 때 발생하는 에러 정의
    INVALID_REFRESH_TOKEN

--- src/main/java/org/websoso/WSSServer/exception/error/CustomAuthError.java
+++ src/main/java/org/websoso/WSSServer/exception/error/CustomAuthError.java
@@ -12,7 +12,8 @@ import org.websoso.WSSServer.exception.common.ICustomError;
 public enum CustomAuthError implements ICustomError {
 
     EXPIRED_REFRESH_TOKEN("AUTH-001", "만료된 리프레시 토큰입니다.", UNAUTHORIZED),
-    INVALID_TOKEN("AUTH-002", "유효하지 않은 토큰입니다.", UNAUTHORIZED);
+    INVALID_TOKEN("AUTH-002", "유효하지 않은 토큰입니다.", UNAUTHORIZED),
+    INVALID_REFRESH_TOKEN("AUTH-003", "등록되지 않은 리프레시 토큰입니다.", UNAUTHORIZED);
 
     private final String code;
     private final String description;
