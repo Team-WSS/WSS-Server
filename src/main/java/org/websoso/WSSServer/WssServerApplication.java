0f9620dfd9632d1c706fe8417c0de0f3c99daafe
Kim-TaeUk <xodnr0130@naver.com>
2024-09-11 01:43:31 +0900

    [FEAT] Application 클래스에 @EnableRedisRepositories 추가

--- src/main/java/org/websoso/WSSServer/WssServerApplication.java
+++ src/main/java/org/websoso/WSSServer/WssServerApplication.java
@@ -2,9 +2,11 @@ package org.websoso.WSSServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableRedisRepositories
 @SpringBootApplication
 public class WssServerApplication {
 
