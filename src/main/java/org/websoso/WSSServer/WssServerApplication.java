321299f084adb132b5b397147d5420225df5cf39
Kim-TaeUk <xodnr0130@naver.com>
2024-09-11 02:34:40 +0900

    [FIX] hash 데이터 TTL 만료 시, set 데이터도 삭제되도록 Redis keyspace 이벤트 활성화

--- src/main/java/org/websoso/WSSServer/WssServerApplication.java
+++ src/main/java/org/websoso/WSSServer/WssServerApplication.java
@@ -2,11 +2,12 @@ package org.websoso.WSSServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableRedisRepositories
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @SpringBootApplication
 public class WssServerApplication {
 
