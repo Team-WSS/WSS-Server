618f13f969ddcecb835527ef79ef19a33ac49e7c
Kim-TaeUk <xodnr0130@naver.com>
2024-09-12 14:05:46 +0900

    [FIX] 관심글 조회 응답 프로퍼티 변경
    관심 소설이 없는 경우와 관심 소설은 있지만 연관된 피드가 없는 경우 분리를 위해 message 프로퍼티 추가

--- src/main/java/org/websoso/WSSServer/dto/feed/InterestFeedsGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/feed/InterestFeedsGetResponse.java
@@ -3,10 +3,15 @@ package org.websoso.WSSServer.dto.feed;
 import java.util.List;
 
 public record InterestFeedsGetResponse(
-        List<InterestFeedGetResponse> recommendFeeds
+        List<InterestFeedGetResponse> recommendFeeds,
+        String message
 ) {
 
     public static InterestFeedsGetResponse of(List<InterestFeedGetResponse> interestFeedGetResponses) {
-        return new InterestFeedsGetResponse(interestFeedGetResponses);
+        return new InterestFeedsGetResponse(interestFeedGetResponses, null);
+    }
+
+    public static InterestFeedsGetResponse of(List<InterestFeedGetResponse> interestFeedGetResponses, String message) {
+        return new InterestFeedsGetResponse(interestFeedGetResponses, message);
     }
 }
