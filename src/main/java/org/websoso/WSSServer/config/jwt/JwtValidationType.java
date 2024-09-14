e42e17ab25aa76724f899ac8c2635b10b1bfc650
Kim-TaeUk <xodnr0130@naver.com>
2024-09-14 21:52:45 +0900

    [REMOVE] 불필요한 주석 제거

--- src/main/java/org/websoso/WSSServer/config/jwt/JwtValidationType.java
+++ src/main/java/org/websoso/WSSServer/config/jwt/JwtValidationType.java
@@ -1,10 +1,10 @@
 package org.websoso.WSSServer.config.jwt;
 
 public enum JwtValidationType {
-    VALID_TOKEN,              // 유효한 JWT
-    INVALID_SIGNATURE,      // 유효하지 않은 서명
-    INVALID_TOKEN,          // 유효하지 않은 토큰
-    EXPIRED_TOKEN,          // 만료된 토큰
-    UNSUPPORTED_TOKEN,      // 지원하지 않는 형식의 토큰
-    EMPTY_TOKEN                   // 빈 JWT
+    VALID_TOKEN,
+    INVALID_SIGNATURE,
+    INVALID_TOKEN,
+    EXPIRED_TOKEN,
+    UNSUPPORTED_TOKEN,
+    EMPTY_TOKEN
 }
