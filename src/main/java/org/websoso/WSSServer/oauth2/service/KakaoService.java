e1c2d4d014a69ced31d5d86d45641ad4dbfb6854
Kim-TaeUk <xodnr0130@naver.com>
2024-10-07 17:24:27 +0900

    [FEAT] 카카오 리소스 서버에서 유저 정보 조회 로직
    RestClient 사용

--- src/main/java/org/websoso/WSSServer/oauth2/service/KakaoService.java
+++ src/main/java/org/websoso/WSSServer/oauth2/service/KakaoService.java
@@ -2,7 +2,11 @@ package org.websoso.WSSServer.oauth2.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+import org.websoso.WSSServer.dto.auth.AuthResponse;
+import org.websoso.WSSServer.oauth2.dto.KakaoUserInfo;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +15,14 @@ public class KakaoService {
     @Value("${kakao.user-info-url}")
     private String kakaoUserInfoUrl;
 
+    public AuthResponse getUserInfoFromKakao(String kakaoAccessToken) {
+        RestClient restClient = RestClient.create();
+
+        KakaoUserInfo kakaoUserInfo = restClient.get()
+                .uri(kakaoUserInfoUrl)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + kakaoAccessToken)
+                .retrieve()
+                .body(KakaoUserInfo.class);
+
+    }
 }
