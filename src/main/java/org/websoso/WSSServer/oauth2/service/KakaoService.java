3f7df5559f6345f02ac38eed4309d3b070956689
Kim-TaeUk <xodnr0130@naver.com>
2024-10-08 00:38:17 +0900

    [FEAT] RestClient에서 카카오 리소스 서버 400번대 에러 핸들링 추가

--- src/main/java/org/websoso/WSSServer/oauth2/service/KakaoService.java
+++ src/main/java/org/websoso/WSSServer/oauth2/service/KakaoService.java
@@ -1,11 +1,15 @@
 package org.websoso.WSSServer.oauth2.service;
 
+import static org.websoso.WSSServer.exception.error.CustomKakaoError.INVALID_KAKAO_ACCESS_TOKEN;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.websoso.WSSServer.dto.auth.AuthResponse;
+import org.websoso.WSSServer.exception.exception.CustomKakaoException;
 import org.websoso.WSSServer.oauth2.dto.KakaoUserInfo;
 
 @Service
@@ -22,6 +26,10 @@ public class KakaoService {
                 .uri(kakaoUserInfoUrl)
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + kakaoAccessToken)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    throw new CustomKakaoException(INVALID_KAKAO_ACCESS_TOKEN,
+                            "given access token from kakao is invalid");
+                })
                 .body(KakaoUserInfo.class);
 
     }
