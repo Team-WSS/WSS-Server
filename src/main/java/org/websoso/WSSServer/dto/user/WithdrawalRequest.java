35f29c1751621204b4a70a3d139c5f138a827a23
Kim-TaeUk <xodnr0130@naver.com>
2024-11-11 23:43:31 +0900

    [FIX] refreshToken에 validation 추가
    @NotBlank

--- src/main/java/org/websoso/WSSServer/dto/user/WithdrawalRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/user/WithdrawalRequest.java
@@ -1,11 +1,13 @@
 package org.websoso.WSSServer.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record WithdrawalRequest(
         @Size(max = 80, message = "탈퇴 사유는 80자를 초과할 수 없습니다.")
         String reason,
 
+        @NotBlank
         String refreshToken
 ) {
 }
