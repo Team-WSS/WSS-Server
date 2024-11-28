9baaf106027430e2fd4b58693ffd1083abdb2be2
ChaeAg <dydkglel123@gmail.com>
2024-11-28 13:57:51 +0900

    [FEAT] 최소 지원 버전 업데이트 API 구현

--- src/main/java/org/websoso/WSSServer/controller/AppVersionController.java
+++ src/main/java/org/websoso/WSSServer/controller/AppVersionController.java
@@ -1,13 +1,18 @@
 package org.websoso.WSSServer.controller;
 
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.websoso.WSSServer.dto.AppVersion.MinimumVersionGetResponse;
+import org.websoso.WSSServer.dto.AppVersion.MinimumVersionUpdateRequest;
 import org.websoso.WSSServer.service.AppVersionService;
 
 @RestController
@@ -21,5 +26,13 @@ public class AppVersionController {
         return ResponseEntity
                 .status(OK)
                 .body(appVersionService.getMinimumVersion(os));
+    }
+
+    @PostMapping("/minimum-version")
+    public ResponseEntity<Void> updateMinimumVersion(@Valid @RequestBody MinimumVersionUpdateRequest request) {
+        appVersionService.updateMinimumVersion(request);
+        return ResponseEntity
+                .status(NO_CONTENT)
+                .build();
     }
 }
--- src/main/java/org/websoso/WSSServer/domain/MinimumVersion.java
+++ src/main/java/org/websoso/WSSServer/domain/MinimumVersion.java
@@ -4,16 +4,20 @@ import static jakarta.persistence.EnumType.STRING;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.websoso.WSSServer.domain.common.OS;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MinimumVersion {
 
@@ -25,6 +29,21 @@ public class MinimumVersion {
     @Column(columnDefinition = "varchar(20)", nullable = false)
     private String minimumVersion;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime updateDate;
+
+    private MinimumVersion(OS os, String minimumVersion) {
+        this.os = os;
+        this.minimumVersion = minimumVersion;
+        this.updateDate = LocalDateTime.now();
+    }
+
+    public static MinimumVersion create(OS os, String minimumVersion) {
+        return new MinimumVersion(os, minimumVersion);
+    }
+
+    public void updateMinimumVersion(String minimumVersion) {
+        this.minimumVersion = minimumVersion;
+    }
 }
