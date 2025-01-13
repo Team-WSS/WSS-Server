3a130acd218fc93988b3f640099553744e26eff2
Kim-TaeUk <xodnr0130@naver.com>
2025-01-13 23:36:13 +0900

    [REFACTOR] Firebase 설정 경로 @Value를 통해 외부에서 주입하도록 수정
    - 하드 코딩된 ClassPathResource를 주입하여 대체
    - Firebase 설정 경로를 yml에서 관리

--- src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,10 +12,13 @@ import org.springframework.core.io.ClassPathResource;
 @Configuration
 public class FCMConfig {
 
+    @Value("${fcm.key-path}")
+    private String firebaseConfigPath;
+
     @Bean
     public FirebaseMessaging firebaseMessaging() {
         GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource("websoso-fcm.json").getInputStream());
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
 
         FirebaseOptions firebaseOptions = FirebaseOptions.builder()
                 .setCredentials(googleCredentials)
