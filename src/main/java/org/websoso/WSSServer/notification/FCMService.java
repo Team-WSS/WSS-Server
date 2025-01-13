8460aa8eabd449dd5674ebbdab80aeed6ffc8a1b
Kim-TaeUk <xodnr0130@naver.com>
2025-01-13 23:42:51 +0900

    [CHORE] FirebaseMessaging Bean 의존성 주입

--- src/main/java/org/websoso/WSSServer/notification/FCMService.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMService.java
@@ -1,8 +1,13 @@
 package org.websoso.WSSServer.notification;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class FCMService {
+
+    private final FirebaseMessaging firebaseMessaging;
 
 }
