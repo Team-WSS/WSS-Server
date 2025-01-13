ad89ffa6a6d7a01afa03b52b92a23118088421ef
Kim-TaeUk <xodnr0130@naver.com>
2025-01-13 23:32:23 +0900

    [FEAT] Firebase SDK 초기화를 위한 FirebaseOptions 추가
    - GoogleCredentials(인증 객체) 설정을 통한 Firebase 인증 정보 구성

--- src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
@@ -1,6 +1,7 @@
 package org.websoso.WSSServer.notification;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
@@ -18,5 +19,9 @@ public class FCMConfig {
         FirebaseOptions firebaseOptions = FirebaseOptions.builder()
                 .setCredentials(googleCredentials)
                 .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(firebaseOptions);
+        }
     }
 }
