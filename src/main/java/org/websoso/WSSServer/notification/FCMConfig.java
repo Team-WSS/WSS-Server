aa387cf2159c0f3b8d963f85607195628d74d596
Kim-TaeUk <xodnr0130@naver.com>
2025-01-13 23:18:21 +0900

    [FEAT] Firebase SDK 인증 설정을 위한 GoogleCredentials 생성
    - Firebase 서비스 계정 key-file 기반 GoogleCredentials(인증 객체) 생성

--- src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
@@ -1,14 +1,17 @@
 package org.websoso.WSSServer.notification;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class FCMConfig {
 
     @Bean
     public FirebaseMessaging firebaseMessaging() {
-
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource("websoso-fcm.json").getInputStream());
     }
 }
