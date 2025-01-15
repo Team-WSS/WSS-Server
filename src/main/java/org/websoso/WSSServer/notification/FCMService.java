6286cedd72a35287e41eed56c72c4a0a7d492e5e
Kim-TaeUk <xodnr0130@naver.com>
2025-01-16 01:20:27 +0900

    [CHORE] logging을 위한 Slf4j 추가

--- src/main/java/org/websoso/WSSServer/notification/FCMService.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMService.java
@@ -8,8 +8,10 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FCMService {
