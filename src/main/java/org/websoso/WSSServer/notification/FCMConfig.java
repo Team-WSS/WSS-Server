1658b73e681a557855d7690a9a0038f632b95135
Kim-TaeUk <xodnr0130@naver.com>
2025-01-16 02:25:37 +0900

    [FIX] FirebaseMessaging Bean 등록 실패 시, 예외 throw 및 logging 추가
    - try-catch로 getInputStream()에서 발생할 수 있는 IOException 잡고, 로깅 및 IllegalStateException throw

--- src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,17 +20,22 @@ public class FCMConfig {
 
     @Bean
     public FirebaseMessaging firebaseMessaging() {
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
 
-        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
-                .setCredentials(googleCredentials)
-                .build();
+            FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                    .setCredentials(googleCredentials)
+                    .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(firebaseOptions);
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(firebaseOptions);
+            }
+
+            return FirebaseMessaging.getInstance();
+        } catch (IOException e) {
+            log.error("[FirebaseMessaging] Failed to initialize FirebaseMessaging", e);
+            throw new IllegalStateException("Failed to initialize FirebaseMessaging due to firebase key file", e);
         }
-
-        return FirebaseMessaging.getInstance();
     }
 }
