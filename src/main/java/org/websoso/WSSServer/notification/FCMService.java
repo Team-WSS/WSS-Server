330334c185fb6b105e8997ecd43aa7061e7d86f4
Kim-TaeUk <xodnr0130@naver.com>
2025-01-15 23:43:43 +0900

    [FEAT] Message에 필요한 AndroidConfig 추가
    - platform-specific한 Message 부분 정의

--- src/main/java/org/websoso/WSSServer/notification/FCMService.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMService.java
@@ -1,5 +1,7 @@
 package org.websoso.WSSServer.notification;
 
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.AndroidNotification;
 import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,14 @@ public class FCMService {
 
     private final FirebaseMessaging firebaseMessaging;
 
+    public void sendPushMessage(String targetFCMToken, String title, String body, String clickAction) {
+        AndroidConfig androidConfig = AndroidConfig.builder()
+                .setNotification(AndroidNotification.builder()
+                        .setTitle(title)
+                        .setBody(body)
+                        .setClickAction(clickAction)
+                        .build()
+                )
+                .build();
+    }
 }
