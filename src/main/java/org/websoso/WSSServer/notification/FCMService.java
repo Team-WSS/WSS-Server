4a19453a9bd4cc72e24b00e1997e7f6093949f2d
Kim-TaeUk <xodnr0130@naver.com>
2025-01-16 01:20:48 +0900

    [FEAT] firebaseMessaging.send()에서 발생하는 Exception 처리
    - FirebaseMessagingException를 try-catch로 잡아서 log 남김

--- src/main/java/org/websoso/WSSServer/notification/FCMService.java
+++ src/main/java/org/websoso/WSSServer/notification/FCMService.java
@@ -5,6 +5,7 @@ import com.google.firebase.messaging.AndroidNotification;
 import com.google.firebase.messaging.ApnsConfig;
 import com.google.firebase.messaging.Aps;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
@@ -19,31 +20,35 @@ public class FCMService {
     private final FirebaseMessaging firebaseMessaging;
 
     public void sendPushMessage(String targetFCMToken, String title, String body, String clickAction) {
-        AndroidConfig androidConfig = AndroidConfig.builder()
-                .setNotification(AndroidNotification.builder()
-                        .setTitle(title)
-                        .setBody(body)
-                        .setClickAction(clickAction)
-                        .build()
-                )
-                .build();
+        try {
+            AndroidConfig androidConfig = AndroidConfig.builder()
+                    .setNotification(AndroidNotification.builder()
+                            .setTitle(title)
+                            .setBody(body)
+                            .setClickAction(clickAction)
+                            .build()
+                    )
+                    .build();
 
-        ApnsConfig apnsConfig = ApnsConfig.builder()
-                .setAps(Aps.builder()
-                        .setCategory(clickAction)
-                        .build())
-                .build();
+            ApnsConfig apnsConfig = ApnsConfig.builder()
+                    .setAps(Aps.builder()
+                            .setCategory(clickAction)
+                            .build())
+                    .build();
 
-        Message message = Message.builder()
-                .setToken(targetFCMToken)
-                .setNotification(Notification.builder()
-                        .setTitle(title)
-                        .setBody(body)
-                        .build())
-                .setAndroidConfig(androidConfig)
-                .setApnsConfig(apnsConfig)
-                .build();
+            Message message = Message.builder()
+                    .setToken(targetFCMToken)
+                    .setNotification(Notification.builder()
+                            .setTitle(title)
+                            .setBody(body)
+                            .build())
+                    .setAndroidConfig(androidConfig)
+                    .setApnsConfig(apnsConfig)
+                    .build();
 
-        firebaseMessaging.send(message);
+            firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            log.error("[FirebaseMessagingException] exception ", e);
+        }
     }
 }
