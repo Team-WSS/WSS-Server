77223144a94c43e1785f41fcfe3a7719d5563c5d
nakyeong <rinarina0429@naver.com>
2025-01-16 19:48:51 +0900

    [FEAT] 알림 단건 조회 dto

---
+++ src/main/java/org/websoso/WSSServer/dto/notification/NotificationGetResponse.java
@@ -0,0 +1,20 @@
+package org.websoso.WSSServer.dto.notification;
+
+import java.time.format.DateTimeFormatter;
+import org.websoso.WSSServer.domain.Notification;
+
+public record NotificationGetResponse(
+        String notificationTitle,
+        String notificationCreatedDate,
+        String notificationDescription
+) {
+    private static final String DATE_PATTERN = "yyyy.MM.dd";
+
+    static public NotificationGetResponse of(Notification notification) {
+        return new NotificationGetResponse(
+                notification.getNotificationTitle(),
+                notification.getCreatedDate().format(DateTimeFormatter.ofPattern(DATE_PATTERN)),
+                notification.getNotificationDescription()
+        );
+    }
+}
