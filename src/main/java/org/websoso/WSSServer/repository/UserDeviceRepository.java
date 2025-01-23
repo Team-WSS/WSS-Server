c17bc6edd87c734256b18ad35867c69d9d41e364
Kim-TaeUk <xodnr0130@naver.com>
2025-01-23 22:18:18 +0900

    [FEAT] UserDeviceRepository에 findByDeviceIdentifier 추가

--- src/main/java/org/websoso/WSSServer/repository/UserDeviceRepository.java
+++ src/main/java/org/websoso/WSSServer/repository/UserDeviceRepository.java
@@ -1,5 +1,6 @@
 package org.websoso.WSSServer.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.websoso.WSSServer.domain.UserDevice;
@@ -7,4 +8,5 @@ import org.websoso.WSSServer.domain.UserDevice;
 @Repository
 public interface UserDeviceRepository extends JpaRepository<UserDevice, Long> {
 
+    Optional<UserDevice> findByDeviceIdentifier(String deviceIdentifier);
 }
