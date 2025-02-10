282d89adfdd2d66e00d58a45a0d845f6c1e8ad2f
nakyeong <rinarina0429@naver.com>
2025-02-10 16:57:04 +0900

    [FEAT] 약관 동의 dto

---
+++ src/main/java/org/websoso/WSSServer/dto/user/ConsentSettingRequest.java
@@ -0,0 +1,13 @@
+package org.websoso.WSSServer.dto.user;
+
+import jakarta.validation.constraints.NotNull;
+
+public record ConsentSettingRequest(
+        @NotNull(message = "웹소소 이용약관 동의 여부는 null일 수 없습니다.")
+        Boolean isTermsAgreed,
+        @NotNull(message = "개인정보 수집 동의 여부는 null일 수 없습니다.")
+        Boolean isPrivacyConsented,
+        @NotNull(message = "마케팅 활용 및 이벤트 정보 수신 동의 여부는 null일 수 없습니다.")
+        Boolean isMarketingConsented
+) {
+}
