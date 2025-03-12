ddafe037a50d870f7cdf130b33a5121a8c931ec1
Kim-TaeUk <xodnr0130@naver.com>
2025-03-13 01:35:23 +0900

    [FEAT] ResourceAuthorizationHandler에서 ResourceAuthorizationValidator 자동 매핑 기능 추가
    @Autowired를 통해 List<ResourceAuthorizationValidator>를 주입, for loop를 통해 validatorMap에 validator의 resourceType을 key로 저장

--- src/main/java/org/websoso/WSSServer/auth/ResourceAuthorizationHandler.java
+++ src/main/java/org/websoso/WSSServer/auth/ResourceAuthorizationHandler.java
@@ -1,7 +1,9 @@
 package org.websoso.WSSServer.auth;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.websoso.WSSServer.auth.validator.ResourceAuthorizationValidator;
 
@@ -10,4 +12,10 @@ public class ResourceAuthorizationHandler {
 
     private final Map<Class<?>, ResourceAuthorizationValidator> validatorMap = new HashMap<>();
 
+    @Autowired
+    public ResourceAuthorizationHandler(List<ResourceAuthorizationValidator> validators) {
+        for (ResourceAuthorizationValidator validator : validators) {
+            validatorMap.put(validator.getResourceType(), validator);
+        }
+    }
 }
