eaa546cf6cf4379aa03036c8e02809c76237100e
Kim-TaeUk <xodnr0130@naver.com>
2025-03-14 00:26:20 +0900

    [FEAT] AuthorizationService에 UserRepository 주입
    by @RequiredArgsConstructor

--- src/main/java/org/websoso/WSSServer/auth/AuthorizationService.java
+++ src/main/java/org/websoso/WSSServer/auth/AuthorizationService.java
@@ -1,9 +1,14 @@
 package org.websoso.WSSServer.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.websoso.WSSServer.repository.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class AuthorizationService {
+
+    private final UserRepository userRepository;
 
     public boolean validate(Long resourceId, Long userId, Class<?> resourceType) {
 
