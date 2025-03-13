3709b577c01a2900d1457cb438636a1fd228df16
Kim-TaeUk <xodnr0130@naver.com>
2025-03-14 00:28:39 +0900

    [FEAT] 사용자 존재 여부 검증 로직 추가

--- src/main/java/org/websoso/WSSServer/auth/AuthorizationService.java
+++ src/main/java/org/websoso/WSSServer/auth/AuthorizationService.java
@@ -1,7 +1,11 @@
 package org.websoso.WSSServer.auth;
 
+import static org.websoso.WSSServer.exception.error.CustomUserError.USER_NOT_FOUND;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.exception.exception.CustomUserException;
 import org.websoso.WSSServer.repository.UserRepository;
 
 @Service
@@ -11,6 +15,7 @@ public class AuthorizationService {
     private final UserRepository userRepository;
 
     public boolean validate(Long resourceId, Long userId, Class<?> resourceType) {
-
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomUserException(USER_NOT_FOUND, "user with the given id was not found"));
     }
 }
