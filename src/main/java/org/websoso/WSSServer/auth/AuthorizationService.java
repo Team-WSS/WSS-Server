47456decc414711cf95e6c16c1a917010ec04816
Kim-TaeUk <xodnr0130@naver.com>
2025-03-14 00:25:08 +0900

    [FEAT] AuthorizationService 추가

---
+++ src/main/java/org/websoso/WSSServer/auth/AuthorizationService.java
@@ -0,0 +1,11 @@
+package org.websoso.WSSServer.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService {
+
+    public boolean validate(Long resourceId, Long userId, Class<?> resourceType) {
+
+    }
+}
