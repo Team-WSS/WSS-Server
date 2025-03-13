16176e6d952172d1c36186cb5ddb7c5a2631925a
Kim-TaeUk <xodnr0130@naver.com>
2025-03-14 00:07:53 +0900

    [FEAT] FeedRepository 주입 by @RequiredArgsConstructor

--- src/main/java/org/websoso/WSSServer/auth/validator/FeedAuthorizationValidator.java
+++ src/main/java/org/websoso/WSSServer/auth/validator/FeedAuthorizationValidator.java
@@ -1,10 +1,15 @@
 package org.websoso.WSSServer.auth.validator;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.repository.FeedRepository;
 
 @Component
+@RequiredArgsConstructor
 public class FeedAuthorizationValidator implements ResourceAuthorizationValidator {
+
+    private final FeedRepository feedRepository;
 
     @Override
     public boolean hasPermission(Long resourceId, User user) {
