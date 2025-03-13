f861975fdc560743608661181fb8759a11f81864
Kim-TaeUk <xodnr0130@naver.com>
2025-03-14 00:12:37 +0900

    [FEAT] 존재하는 리소스인지 조회

--- src/main/java/org/websoso/WSSServer/auth/validator/FeedAuthorizationValidator.java
+++ src/main/java/org/websoso/WSSServer/auth/validator/FeedAuthorizationValidator.java
@@ -1,9 +1,12 @@
 package org.websoso.WSSServer.auth.validator;
 
+import static org.websoso.WSSServer.exception.error.CustomFeedError.FEED_NOT_FOUND;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.websoso.WSSServer.domain.Feed;
 import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.exception.exception.CustomFeedException;
 import org.websoso.WSSServer.repository.FeedRepository;
 
 @Component
@@ -13,8 +16,9 @@ public class FeedAuthorizationValidator implements ResourceAuthorizationValidato
     private final FeedRepository feedRepository;
 
     @Override
-    public boolean hasPermission(Long resourceId, User user) {
-        return false;
+    public boolean hasPermission(Long feedId, User user) {
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> new CustomFeedException(FEED_NOT_FOUND, "feed with the given id was not found"));
     }
 
     @Override
