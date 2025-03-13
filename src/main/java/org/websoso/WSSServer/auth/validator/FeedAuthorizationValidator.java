27e70821184b8800d54c458d823efb7c81cd308d
Kim-TaeUk <xodnr0130@naver.com>
2025-03-14 00:19:33 +0900

    [FEAT] 권한이 없는 경우 예외 throw
    권한 X = 본인의 리소스가 아닌 경우

--- src/main/java/org/websoso/WSSServer/auth/validator/FeedAuthorizationValidator.java
+++ src/main/java/org/websoso/WSSServer/auth/validator/FeedAuthorizationValidator.java
@@ -1,12 +1,14 @@
 package org.websoso.WSSServer.auth.validator;
 
 import static org.websoso.WSSServer.exception.error.CustomFeedError.FEED_NOT_FOUND;
+import static org.websoso.WSSServer.exception.error.CustomUserError.INVALID_AUTHORIZED;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.websoso.WSSServer.domain.Feed;
 import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.exception.exception.CustomFeedException;
+import org.websoso.WSSServer.exception.exception.CustomUserException;
 import org.websoso.WSSServer.repository.FeedRepository;
 
 @Component
@@ -19,6 +21,13 @@ public class FeedAuthorizationValidator implements ResourceAuthorizationValidato
     public boolean hasPermission(Long feedId, User user) {
         Feed feed = feedRepository.findById(feedId)
                 .orElseThrow(() -> new CustomFeedException(FEED_NOT_FOUND, "feed with the given id was not found"));
+
+        if (!feed.getUser().getUserId()
+                .equals(user.getUserId())) {
+            throw new CustomUserException(INVALID_AUTHORIZED,
+                    "User with ID " + user.getUserId() + " is not the owner of feed " + feed.getFeedId());
+        }
+        return true;
     }
 
     @Override
