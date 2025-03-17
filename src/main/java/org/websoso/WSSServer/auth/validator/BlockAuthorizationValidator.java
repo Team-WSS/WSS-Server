aaa92848caa5f4c13d3cd77d561d53e4a33bc457
Kim-TaeUk <xodnr0130@naver.com>
2025-03-18 00:23:48 +0900

    [FEAT] BlockAuthorizationValidator 추가

---
+++ src/main/java/org/websoso/WSSServer/auth/validator/BlockAuthorizationValidator.java
@@ -0,0 +1,44 @@
+package org.websoso.WSSServer.auth.validator;
+
+import static org.websoso.WSSServer.exception.error.CustomBlockError.BLOCK_NOT_FOUND;
+import static org.websoso.WSSServer.exception.error.CustomBlockError.INVALID_AUTHORIZED_BLOCK;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.websoso.WSSServer.domain.Block;
+import org.websoso.WSSServer.domain.User;
+import org.websoso.WSSServer.exception.exception.CustomBlockException;
+import org.websoso.WSSServer.repository.BlockRepository;
+
+@Component
+@RequiredArgsConstructor
+public class BlockAuthorizationValidator implements ResourceAuthorizationValidator {
+
+    private final BlockRepository blockRepository;
+
+    @Override
+    public boolean hasPermission(Long blockId, User user) {
+        Block block = getBlockOrException(blockId);
+
+        if (!isBlockOwner(block, user)) {
+            throw new CustomBlockException(INVALID_AUTHORIZED_BLOCK,
+                    "block with the given blockId is not from user with the given userId");
+        }
+        return true;
+    }
+
+    private Block getBlockOrException(Long blockId) {
+        return blockRepository.findById(blockId)
+                .orElseThrow(
+                        () -> new CustomBlockException(BLOCK_NOT_FOUND, "block with the given blockId was not found"));
+    }
+
+    private boolean isBlockOwner(Block block, User user) {
+        return block.getBlockedId().equals(user.getUserId());
+    }
+
+    @Override
+    public Class<?> getResourceType() {
+        return Block.class;
+    }
+}
