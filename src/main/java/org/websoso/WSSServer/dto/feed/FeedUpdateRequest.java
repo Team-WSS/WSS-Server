7e64d0253e898686edcdd24c8118c1b04a3d52b2
Kim-TaeUk <xodnr0130@naver.com>
2025-04-18 20:34:28 +0900

    [STYLE] 가독성을 위한 개행 - FeedUpdateRequest

--- src/main/java/org/websoso/WSSServer/dto/feed/FeedUpdateRequest.java
+++ src/main/java/org/websoso/WSSServer/dto/feed/FeedUpdateRequest.java
@@ -10,10 +10,13 @@ public record FeedUpdateRequest(
         @NotNull(message = "카테고리는 null일 수 없습니다.")
         @NotEmpty(message = "카테고리는 1개 이상 선택해야 합니다.")
         List<String> relevantCategories,
+
         @NotBlank(message = "피드 내용은 비어 있거나, 공백일 수 없습니다.")
         @Size(max = 2000, message = "피드 내용은 2000자를 초과할 수 없습니다.")
         String feedContent,
+
         Long novelId,
+
         @NotNull(message = "스포일러 여부는 null일 수 없습니다.")
         Boolean isSpoiler
 ) {
