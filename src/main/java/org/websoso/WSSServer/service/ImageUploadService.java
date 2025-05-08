b1d43ed6b82b2eaef8496932e3e74b85664a5ef1
GiJungPark <weeds2577@gmail.com>
2025-05-09 02:06:36 +0900

    [FEAT] ImageUploadService 작성
    S3 버킷의 feed 폴더에 이미지를 업로드합니다.
    이미지 파일의 이름은 UUID.{기존 이미지 확장자} 입니다.
    성공적으로 업로드시 이미지가 업로드된 URL을 제공하며, 실패시 예외가 발생합니다.
    또한 다음과 같은 유효성을 검증합니다.
    - 빈 파일 여부
    - 파일 확장자 여부

---
+++ src/main/java/org/websoso/WSSServer/service/ImageUploadService.java
@@ -0,0 +1,52 @@
+package org.websoso.WSSServer.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.websoso.s3.core.S3FileService;
+import org.websoso.s3.modle.S3UploadResult;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+//TODO: Exception 수정해야함
+@Service
+@RequiredArgsConstructor
+public class ImageUploadService {
+
+    private static final String FEED_UPLOAD_DIRECTORY = "feed/";
+
+    private final S3FileService s3FileService;
+
+    public String uploadFeedImage(MultipartFile file) {
+
+        if (file == null || file.isEmpty()) {
+            throw new IllegalArgumentException("빈 파일은 업로드할 수 없습니다.");
+        }
+
+        String originalFilename = file.getOriginalFilename();
+
+        if (originalFilename == null || !originalFilename.contains(".")) {
+            throw new IllegalArgumentException("유효하지 않은 파일 이름입니다.");
+        }
+
+        String extension = originalFilename.substring(originalFilename.lastIndexOf('.')); // 예: ".jpg"
+        String imageName = UUID.randomUUID() + extension;
+        String key = FEED_UPLOAD_DIRECTORY + imageName;
+
+        try (InputStream inputStream = file.getInputStream()) {
+            S3UploadResult result = s3FileService.upload(
+                    key,
+                    inputStream,
+                    file.getContentType(),
+                    file.getSize()
+            );
+
+            // TODO: Library의 로직 문제로 수정시 리팩토링 예정입니다.
+            return result.message();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("이미지 업로드 중 오류가 발생했습니다.", e);
+        }
+    }
+}
