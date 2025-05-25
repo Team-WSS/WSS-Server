8b5ab89fe249f5521881ca0afb40ab6c5e2ec147
rinarina0429 <forrina0429@gmail.com>
2025-05-25 18:11:48 +0900

    [FEAT] 서재 조회 위한 dto 내 from() 추가

--- src/main/java/org/websoso/WSSServer/dto/userNovel/UserNovelAndNovelGetResponse.java
+++ src/main/java/org/websoso/WSSServer/dto/userNovel/UserNovelAndNovelGetResponse.java
@@ -1,6 +1,12 @@
 package org.websoso.WSSServer.dto.userNovel;
 
 import java.util.List;
+import org.websoso.WSSServer.domain.AttractivePoint;
+import org.websoso.WSSServer.domain.Keyword;
+import org.websoso.WSSServer.domain.Novel;
+import org.websoso.WSSServer.domain.UserNovel;
+import org.websoso.WSSServer.domain.UserNovelAttractivePoint;
+import org.websoso.WSSServer.domain.UserNovelKeyword;
 
 public record UserNovelAndNovelGetResponse(
         Long userNovelId,
@@ -17,4 +23,33 @@ public record UserNovelAndNovelGetResponse(
         List<String> keywords,
         List<String> myFeeds
 ) {
+    public static UserNovelAndNovelGetResponse from(UserNovel userNovel, Float novelRatingAvg, List<String> feeds) {
+        Novel novel = userNovel.getNovel();
+
+        List<String> attractivePoints = userNovel.getUserNovelAttractivePoints().stream()
+                .map(UserNovelAttractivePoint::getAttractivePoint)
+                .map(AttractivePoint::getAttractivePointName)
+                .toList();
+
+        List<String> keywords = userNovel.getUserNovelKeywords().stream()
+                .map(UserNovelKeyword::getKeyword)
+                .map(Keyword::getKeywordName)
+                .toList();
+
+        return new UserNovelAndNovelGetResponse(
+                userNovel.getUserNovelId(),
+                novel.getNovelId(),
+                novel.getTitle(),
+                novel.getNovelImage(),
+                novelRatingAvg,
+                userNovel.getStatus() != null ? userNovel.getStatus().name() : null,
+                userNovel.getIsInterest(),
+                userNovel.getUserNovelRating(),
+                attractivePoints,
+                userNovel.getStartDate() != null ? userNovel.getStartDate().toString() : null,
+                userNovel.getEndDate() != null ? userNovel.getEndDate().toString() : null,
+                keywords,
+                feeds
+        );
+    }
 }
