e56f0b7eb107063df7bc74792b8dc2a37e46cbd7
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-07-08 23:09:07 +0900

    [FIX] 장르 표현식 중복 제거

--- src/main/java/org/websoso/WSSServer/repository/FeedCustomRepositoryImpl.java
+++ src/main/java/org/websoso/WSSServer/repository/FeedCustomRepositoryImpl.java
@@ -65,7 +65,6 @@ public class FeedCustomRepositoryImpl implements FeedCustomRepository, FeedImage
                 .where(
                         feed.user.eq(owner),
                         ltFeedId(lastFeedId),
-                        checkGenres(genres),
                         checkVisible(visitorId),
                         checkPublic(isVisible, isUnVisible),
                         checkGenres(genres)
