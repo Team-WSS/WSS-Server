b9261145877e549739844fdb216dfd1996b64fc2
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-11-25 21:55:58 +0900

    Refactor: CommentController 생성 후 Comment 관련 Handler로직 이동

--- src/main/java/org/websoso/WSSServer/feed/controller/FeedController.java
+++ src/main/java/org/websoso/WSSServer/feed/controller/FeedController.java
@@ -17,16 +17,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.websoso.WSSServer.domain.User;
 import org.websoso.WSSServer.domain.common.FeedGetOption;
-import org.websoso.WSSServer.dto.comment.CommentCreateRequest;
-import org.websoso.WSSServer.dto.comment.CommentUpdateRequest;
-import org.websoso.WSSServer.dto.comment.CommentsGetResponse;
 import org.websoso.WSSServer.dto.feed.FeedCreateRequest;
 import org.websoso.WSSServer.dto.feed.FeedCreateResponse;
 import org.websoso.WSSServer.dto.feed.FeedGetResponse;
@@ -131,51 +127,6 @@ public class FeedController {
         return ResponseEntity
                 .status(OK)
                 .body(feedService.getInterestFeeds(user));
-    }
-
-    @PostMapping("/{feedId}/comments")
-    @PreAuthorize("isAuthenticated() and @feedAccessValidator.canAccess(#feedId, #user)")
-    public ResponseEntity<Void> createComment(@AuthenticationPrincipal User user,
-                                              @PathVariable("feedId") Long feedId,
-                                              @Valid @RequestBody CommentCreateRequest request) {
-        feedService.createComment(user, feedId, request);
-        return ResponseEntity
-                .status(NO_CONTENT)
-                .build();
-    }
-
-    @GetMapping("/{feedId}/comments")
-    @PreAuthorize("isAuthenticated() and @feedAccessValidator.canAccess(#feedId, #user)")
-    public ResponseEntity<CommentsGetResponse> getComments(@AuthenticationPrincipal User user,
-                                                           @PathVariable("feedId") Long feedId) {
-        return ResponseEntity
-                .status(OK)
-                .body(feedService.getComments(user, feedId));
-    }
-
-    @PutMapping("/{feedId}/comments/{commentId}")
-    @PreAuthorize("isAuthenticated() and @feedAccessValidator.canAccess(#feedId, #user) "
-            + "and @authorizationService.validate(#commentId, #user, T(org.websoso.WSSServer.feed.domain.Comment))")
-    public ResponseEntity<Void> updateComment(@AuthenticationPrincipal User user,
-                                              @PathVariable("feedId") Long feedId,
-                                              @PathVariable("commentId") Long commentId,
-                                              @Valid @RequestBody CommentUpdateRequest request) {
-        feedService.updateComment(user, feedId, commentId, request);
-        return ResponseEntity
-                .status(NO_CONTENT)
-                .build();
-    }
-
-    @DeleteMapping("/{feedId}/comments/{commentId}")
-    @PreAuthorize("isAuthenticated() and @feedAccessValidator.canAccess(#feedId, #user) "
-            + "and @authorizationService.validate(#commentId, #user, T(org.websoso.WSSServer.feed.domain.Comment))")
-    public ResponseEntity<Void> deleteComment(@AuthenticationPrincipal User user,
-                                              @PathVariable("feedId") Long feedId,
-                                              @PathVariable("commentId") Long commentId) {
-        feedService.deleteComment(user, feedId, commentId);
-        return ResponseEntity
-                .status(NO_CONTENT)
-                .build();
     }
 
     @PostMapping("/{feedId}/spoiler")
