9c0958e8e5bb9af6c023a9e0776d4ca342419638
GiJungPark <weeds2577@gmail.com>
2026-02-20 13:41:36 +0900

    [FEAT] UserNovel 엔티티 내 관심 전용 여부 확인 메서드 추가

--- src/main/java/org/websoso/WSSServer/library/domain/UserNovel.java
+++ src/main/java/org/websoso/WSSServer/library/domain/UserNovel.java
@@ -123,4 +123,8 @@ public class UserNovel extends BaseEntity {
         this.endDate = null;
     }
 
+    public boolean isOnlyInterested() {
+        return this.status == DEFAULT_STATUS;
+    }
+
 }
