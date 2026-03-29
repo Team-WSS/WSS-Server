1aaba95d5aa97c6048be6103e42cb35d3fcde074
GiJungPark <weeds2577@gmail.com>
2026-03-30 01:17:21 +0900

    [REFACTOR] 알림 시간 포맷팅 메서드를 DTO에서 유틸 클래스로 분리

--- src/main/java/org/websoso/WSSServer/util/TimeFormatUtil.java
+++ src/main/java/org/websoso/WSSServer/util/TimeFormatUtil.java
@@ -4,7 +4,10 @@ import java.time.Clock;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class TimeFormatUtil {
 
     private static final long SECONDS_PER_MINUTE = 60;
@@ -14,7 +17,17 @@ public class TimeFormatUtil {
 
     private static final DateTimeFormatter SAME_YEAR_FMT = DateTimeFormatter.ofPattern("M월 d일");
     private static final DateTimeFormatter DIFF_YEAR_FMT = DateTimeFormatter.ofPattern("yyyy년 M월 d일");
+    private static final DateTimeFormatter NOTIFICATION_FMT = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 
+    /**
+     * 피드 날짜 포맷
+     * 60초 미만: 방금 전
+     * 1시간 미만: N분 전
+     * 24시간 미만: N시간 전
+     * 7일 미만: N일 전
+     * 동일 년도: M월 d일
+     * 과거 년도: yyyy년 M월 d일
+     */
     public static String formatRelativeTime(LocalDateTime createdDate) {
         return formatRelativeTime(createdDate, Clock.systemDefaultZone());
     }
@@ -49,4 +62,38 @@ public class TimeFormatUtil {
 
         return createdDate.format(DIFF_YEAR_FMT);
     }
+
+    /**
+     * 알림 목록용 날짜 포맷
+     * 60초 미만: 방금 전
+     * 1시간 미만: N분 전
+     * 24시간 미만: N시간 전
+     * 24시간 이상: yyyy.MM.dd 형식
+     */
+    public static String formatNotificationDate(LocalDateTime createdDate) {
+        return formatNotificationDate(createdDate, Clock.systemDefaultZone());
+    }
+
+    public static String formatNotificationDate(LocalDateTime createdDate, Clock clock) {
+        LocalDateTime now = LocalDateTime.now(clock);
+        Duration duration = Duration.between(createdDate, now);
+
+        long seconds = duration.getSeconds();
+        if (seconds < SECONDS_PER_MINUTE) {
+            return "방금 전";
+        }
+
+        long minutes = duration.toMinutes();
+        if (minutes < MINUTES_PER_HOUR) {
+            return minutes + "분 전";
+        }
+
+        long hours = duration.toHours();
+        if (hours < HOURS_PER_DAY) {
+            return hours + "시간 전";
+        }
+
+        return createdDate.format(NOTIFICATION_FMT);
+    }
+
 }
